4edbd6d008dd96bc44c7bbce6313cc2e6790e37d
Dawei Li <thomashyde23@fb.com>
2021-05-24 13:55:36 -0700

    Switching to numpy calculation of nowcast features
    Summary: Switching to numpy calculation of nowcast features for efficiency boosting via numba. Seeing a 15X boost in the stressed test (time series data with 1000-5000 data points) on getting nowcast features.
    
    Reviewed By: weizhic, Papa-Panda
    
    Differential Revision: D28616195
    
    fbshipit-source-id: f72196049329bf216b1c77a64185e442cb48dfac

--- kats/tsfeatures/tsfeatures.py
+++ kats/tsfeatures/tsfeatures.py
@@ -23,7 +23,6 @@ from kats.detectors import (
     trend_mk,
     seasonality,
 )
-from kats.models.nowcasting import feature_extraction
 from numba import jit  # @manual
 from scipy import stats
 from scipy.signal import periodogram  # @manual
@@ -119,11 +118,7 @@ class TsFeatures:
         window: int = 5,
         n_fast: int = 12,
         n_slow: int = 21,
-        # pyre-fixme[9]: selected_features has type `Optional[typing.Any]`; used as
-        #  `None`.
-        # pyre-fixme[9]: selected_features has type `Optional[typing.Any]`; used as
-        #  `None`.
-        selected_features: Optional = None,
+        selected_features: Optional[Dict[str, bool]] = None,
         **kwargs,
     ):
         # init hyper-parameters
@@ -260,7 +255,6 @@ class TsFeatures:
         elif selected_features:
             default = False
             self.final_filter = {k: default for k in f2g.keys()}
-            # pyre-fixme[16]: `Optional` has no attribute `__iter__`.
             for f in selected_features:
                 assert (
                     f in f2g.keys() or f in g2f.keys()
@@ -498,7 +492,7 @@ class TsFeatures:
         dict_nowcasting_features = {}
         if self.nowcasting:
             dict_nowcasting_features = self.get_nowcasting(
-                ts,
+                x,
                 window=self.window,
                 n_fast=self.n_fast,
                 n_slow=self.n_slow,
@@ -951,8 +945,7 @@ class TsFeatures:
         x: np.ndarray,
         acfpacf_lag: int = 6,
         period: int = 7,
-        # pyre-fixme[9]: extra_args has type `Optional[typing.Any]`; used as `None`.
-        extra_args: Optional = None,
+        extra_args: Optional[Dict[str, bool]] = None,
         default_status: bool = True,
     ):
         """
@@ -1474,8 +1467,7 @@ class TsFeatures:
         ts: TimeSeriesData,
         decomp: str = "additive",
         iqr_mult: float = 3.0,
-        # pyre-fixme[9]: extra_args has type `Optional[typing.Any]`; used as `None`.
-        extra_args: Optional = None,
+        extra_args: Optional[Dict[str, bool]] = None,
         default_status: bool = True,
     ):
         """
@@ -1500,8 +1492,7 @@ class TsFeatures:
         try:
             odetector = outlier.OutlierDetector(ts, decomp=decomp, iqr_mult=iqr_mult)
             odetector.detector()
-            # pyre-fixme[16]: `Optional` has no attribute `get`.
-            if extra_args.get("outlier_num", default_status):
+            if extra_args is not None and extra_args.get("outlier_num", default_status):
                 # pyre-fixme[16]: `OutlierDetector` has no attribute `outliers`.
                 outlier_detector_features["outlier_num"] = len(odetector.outliers[0])
             return outlier_detector_features
@@ -1565,22 +1556,135 @@ class TsFeatures:
         except Exception:
             return trend_detector_features
 
-    # Nowcasting features (3)
     @staticmethod
-    def get_nowcasting(
-        ts: TimeSeriesData,
+    @jit(nopython=True)
+    def _ewma(
+        arr: np.ndarray,
+        span: int,
+        min_periods: int
+    ):
+        """
+        Exponentialy weighted moving average specified by a decay ``window``
+        to provide better adjustments for small windows via:
+            y[t] = (x[t] + (1-a)*x[t-1] + (1-a)^2*x[t-2] + ... + (1-a)^n*x[t-n]) /
+                   (1 + (1-a) + (1-a)^2 + ... + (1-a)^n).
+
+        Args:
+        arr : np.ndarray; A single dimenisional numpy array.
+        span : int; The decay window, or 'span'.
+        min_periods: int; Minimum amount of data points we'd like to include in the output.
+
+        Returns:
+            A np.ndarray. The exponentially weighted moving average of the array.
+        """
+        output_array = np.empty(arr.shape[0], dtype=np.float64)
+        output_array[:] = np.NaN
+
+        arr = arr[~np.isnan(arr)]
+        n = arr.shape[0]
+        ewma = np.empty(n, dtype=np.float64)
+        alpha = 2 / float(span + 1)
+        w = 1
+        ewma_old = arr[0]
+        ewma[0] = ewma_old
+        for i in range(1, n):
+            w += (1-alpha)**i
+            ewma_old = ewma_old*(1-alpha) + arr[i]
+            ewma[i] = ewma_old / w
+
+        output_subset = ewma[(min_periods-1):]
+        output_array[-len(output_subset):] = output_subset
+        return output_array
+
+    @staticmethod
+    @jit(forceobj=True)
+    def _get_nowcasting_np(
+        x: np.ndarray,
         window: int = 5,
         n_fast: int = 12,
         n_slow: int = 21,
-        # pyre-fixme[9]: extra_args has type `Optional[typing.Any]`; used as `None`.
-        extra_args: Optional = None,
+        extra_args: Optional[Dict[str, bool]] = None,
+        default_status: bool = True,
+    ):
+        """
+        Internal function for actually performing feature engineering using the same procedure as
+        nowcasting feature_extraction under kats/models.
+
+        Args:
+            x: The univariate time series array in the form of 1d numpy array.
+            window: int; Length of window size for all Nowcasting features. Default value is 5.
+            n_fast: int; length of "fast" or short period exponential moving average in the MACD
+                algorithm in the nowcasting features. Default value is 12.
+            n_slow: int; length of "slow" or long period exponential moving average in the MACD
+                algorithm in the nowcasting features. Default value is 21.
+            extra_args: A dictionary containing information for disabling calculation
+                of a certain feature. Default value is None, i.e. no feature is disabled.
+            default_status: Default status of the switch for calculate the features or not.
+                Default value is True.
+
+        Returns:
+            A list containing extracted nowcast features.
+        """
+
+        # initializing the outputs
+        nowcasting_features = [np.nan for _ in range(7)]
+
+        # ROC: indicating return comparing to step n back.
+        if extra_args is not None and extra_args.get("nowcast_roc", default_status):
+            M = x[(window-1):] - x[:-(window-1)]
+            N = x[:-(window-1)]
+            arr = M / N
+            nowcasting_features[0] = np.nan_to_num(arr, nan=0.0, posinf=0.0, neginf=0.0).mean()
+
+        # MOM: indicating momentum: difference of current value and n steps back.
+        if extra_args is not None and extra_args.get("nowcast_mom", default_status):
+            M = x[window:] - x[:-window]
+            nowcasting_features[1] = np.nan_to_num(M, nan=0.0, posinf = 0.0, neginf=0.0).mean()
+
+        # MA: indicating moving average in the past n steps.
+        if extra_args is not None and extra_args.get("nowcast_ma", default_status):
+            ret = np.cumsum(x, dtype=float)
+            ret[window:] = ret[window:] - ret[:-window]
+            ma = ret[window - 1:] / window
+            nowcasting_features[2] = np.nan_to_num(ma, nan=0.0, posinf=0.0, neginf=0.0).mean()
+
+        # LAG: indicating lagged value at the past n steps.
+        if extra_args is not None and extra_args.get("nowcast_lag", default_status):
+            N = x[:-window]
+            nowcasting_features[3] = np.nan_to_num(N, nan=0.0, posinf=0.0, neginf=0.0).mean()
+
+        # MACD: https://www.investopedia.com/terms/m/macd.asp.
+        ema_fast = TsFeatures._ewma(x, n_fast, n_slow-1)
+        ema_slow = TsFeatures._ewma(x, n_slow, n_slow-1)
+        MACD = ema_fast - ema_slow
+        if extra_args is not None and extra_args.get("nowcast_macd", default_status):
+            nowcasting_features[4] = np.nan_to_num(np.nanmean(MACD), nan=0.0, posinf=0.0, neginf=0.0)
+
+        MACDsign = TsFeatures._ewma(MACD, 9, 8)
+        if extra_args is not None and extra_args.get("nowcast_macdsign", default_status):
+            nowcasting_features[5] = np.nan_to_num(np.nanmean(MACDsign), nan=0.0, posinf=0.0, neginf=0.0)
+
+        MACDdiff = MACD - MACDsign
+        if extra_args is not None and extra_args.get("nowcast_macddiff", default_status):
+            nowcasting_features[6] = np.nan_to_num(np.nanmean(MACDdiff), nan=0.0, posinf=0.0, neginf=0.0)
+
+        return nowcasting_features
+
+    # Nowcasting features (7)
+    @staticmethod
+    def get_nowcasting(
+        x: np.ndarray,
+        window: int = 5,
+        n_fast: int = 12,
+        n_slow: int = 21,
+        extra_args: Optional[Dict[str, bool]] = None,
         default_status: bool = True,
     ):
         """
         Run the Kats Nowcasting transformer on the Time Series, extract aggregated features from the outputs of the transformation.
 
         Args:
-            ts: The univariate time series array in the form of Kats TimeSeriesData object.
+            x: The univariate time series array in the form of 1d numpy array.
             window: int; Length of window size for all Nowcasting features. Default value is 5.
             n_fast: int; length of "fast" or short period exponential moving average in the MACD
                 algorithm in the nowcasting features. Default value is 12.
@@ -1598,60 +1702,25 @@ class TsFeatures:
             Momentum (MOM) time series, (4) Mean of LAG time series, (5) Means of MACD, MACDsign,
             and MACDdiff from Kats Nowcasting.
         """
-
         nowcasting_features = {}
-        # pyre-fixme[16]: `Optional` has no attribute `get`.
-        if extra_args.get("nowcast_roc", default_status):
-            nowcasting_features["nowcast_roc"] = np.nan
-        if extra_args.get("nowcast_ma", default_status):
-            nowcasting_features["nowcast_ma"] = np.nan
-        if extra_args.get("nowcast_mom", default_status):
-            nowcasting_features["nowcast_mom"] = np.nan
-        if extra_args.get("nowcast_lag", default_status):
-            nowcasting_features["nowcast_lag"] = np.nan
-        if extra_args.get("nowcast_macd", default_status):
-            nowcasting_features["nowcast_macd"] = np.nan
-        if extra_args.get("nowcast_macdsign", default_status):
-            nowcasting_features["nowcast_macdsign"] = np.nan
-        if extra_args.get("nowcast_macddiff", default_status):
-            nowcasting_features["nowcast_macddiff"] = np.nan
+        features = [
+            "nowcast_roc",
+            "nowcast_mom",
+            "nowcast_ma",
+            "nowcast_lag",
+            "nowcast_macd",
+            "nowcast_macdsign",
+            "nowcast_macddiff",
+        ]
+        for feature in features:
+            if extra_args is not None and extra_args.get(feature, default_status):
+                nowcasting_features[feature] = np.nan
 
         try:
-            ts_df = pd.DataFrame({"y": ts.value, "hist_ds": ts.time})
-            if extra_args.get("nowcast_roc", default_status):
-                feat_name = "ROC_" + str(window)
-                nc_feat = feature_extraction.ROC(df=ts_df, n=window)
-                # pyre-fixme[29]: `Series` is not a function.
-                nowcasting_features["nowcast_roc"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-            if extra_args.get("nowcast_ma", default_status):
-                feat_name = "MA_" + str(window)
-                nc_feat = feature_extraction.MA(df=ts_df, n=window)
-                # pyre-fixme[29]: `Series` is not a function.
-                nowcasting_features["nowcast_ma"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-            if extra_args.get("nowcast_mom", default_status):
-                feat_name = "MOM_" + str(window)
-                nc_feat = feature_extraction.MOM(df=ts_df, n=window)
-                # pyre-fixme[29]: `Series` is not a function.
-                nowcasting_features["nowcast_mom"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-            if extra_args.get("nowcast_lag", default_status):
-                feat_name = "LAG_" + str(window)
-                nc_feat = feature_extraction.LAG(df=ts_df, n=window)
-                # pyre-fixme[29]: `Series` is not a function.
-                nowcasting_features["nowcast_lag"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-            if extra_args.get("nowcast_macd", default_status) or extra_args.get("nowcast_macdsign", default_status) or extra_args.get("nowcast_macddiff", default_status):
-                nc_feat = feature_extraction.MACD(df=ts_df, n_fast=n_fast, n_slow=n_slow)
-                if extra_args.get("nowcast_macd", default_status):
-                    feat_name = "MACD_" + str(n_fast) + "_" + str(n_slow)
-                    # pyre-fixme[29]: `Series` is not a function.
-                    nowcasting_features["nowcast_macd"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-                if extra_args.get("nowcast_macdsign", default_status):
-                    feat_name = "MACDsign_" + str(n_fast) + "_" + str(n_slow)
-                    # pyre-fixme[29]: `Series` is not a function.
-                    nowcasting_features["nowcast_macdsign"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
-                if extra_args.get("nowcast_macddiff", default_status):
-                    feat_name = "MACDdiff_" + str(n_fast) + "_" + str(n_slow)
-                    # pyre-fixme[29]: `Series` is not a function.
-                    nowcasting_features["nowcast_macddiff"] = nc_feat[[feat_name]].mean().fillna(0).replace([np.inf, -np.inf], 0)[feat_name]
+            _features = TsFeatures._get_nowcasting_np(x, window, n_fast, n_slow, extra_args, default_status)
+            for idx, feature in enumerate(features):
+                if extra_args is not None and extra_args.get(feature, default_status):
+                    nowcasting_features[feature] = _features[idx]
             return nowcasting_features
         except Exception:
             return nowcasting_features
