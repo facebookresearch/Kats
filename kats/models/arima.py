016340066e6c36bb20238f14a556473780994e74
Yang Yu <yangbk@fb.com>
2021-06-03 17:26:10 -0700

    Add include_history param to .predict method
    Summary: As title.
    
    Reviewed By: iamxiaodong
    
    Differential Revision: D28879667
    
    fbshipit-source-id: cdb9d705f39b0c334792ea5d492dc5d6b15e71d8

--- kats/models/arima.py
+++ kats/models/arima.py
@@ -16,16 +16,14 @@ We use the implementation in statsmodels <https://www.statsmodels.org/stable/ind
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
+from typing import List, Dict, Optional, Callable, Any
 
 import kats.models.model as m
 import numpy as np
 import pandas as pd
 from kats.consts import Params, TimeSeriesData
+from kats.utils.parameter_tuning_utils import get_default_arima_parameter_search_space
 from statsmodels.tsa.arima_model import ARIMA
-from typing import List, Dict, Optional, Callable, Any
-from kats.utils.parameter_tuning_utils import (
-    get_default_arima_parameter_search_space
-)
 
 
 class ARIMAParams(Params):
@@ -45,13 +43,7 @@ class ARIMAParams(Params):
 
     __slots__ = ["p", "d", "q"]
 
-    def __init__(
-        self,
-        p: int,
-        d: int,
-        q: int,
-        **kwargs
-    ) -> None:
+    def __init__(self, p: int, d: int, q: int, **kwargs) -> None:
         super().__init__()
         self.p = p
         self.d = d
@@ -77,11 +69,7 @@ class ARIMAModel(m.Model):
         params: The ARIMA model parameters from ARIMAParams
     """
 
-    def __init__(
-        self,
-        data: TimeSeriesData,
-        params: ARIMAParams
-    ) -> None:
+    def __init__(self, data: TimeSeriesData, params: ARIMAParams) -> None:
         super().__init__(data, params)
         if not isinstance(self.data.value, pd.Series):
             msg = "Only support univariate time series, but get {type}.".format(
@@ -89,6 +77,17 @@ class ARIMAModel(m.Model):
             )
             logging.error(msg)
             raise ValueError(msg)
+        self.exog = None
+        self.alpha = 0.05
+        self.freq = None
+        self.model = None
+        self.include_history = False
+        self.alpha = 0.05
+        self.fcst_df = None
+        self.freq = None
+        self.y_fcst = None
+        self.y_fcst_lower = None
+        self.y_fcst_upper = None
 
     def fit(
         self,
@@ -156,7 +155,6 @@ class ARIMAModel(m.Model):
             freq=self.params.freq,
         )
         logging.info("Created arima model.")
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `model`.
         self.model = arima.fit(
             start_params=self.start_params,
             transparams=self.transparams,
@@ -172,11 +170,14 @@ class ARIMAModel(m.Model):
         logging.info("Fitted arima.")
 
     # pyre-fixme[14]: `predict` overrides method defined in `Model` inconsistently.
-    def predict(self, steps: int, **kwargs) -> pd.DataFrame:
+    def predict(
+        self, steps: int, include_history: bool = False, **kwargs
+    ) -> pd.DataFrame:
         """Predict with fitted ARIMA model
 
         Args:
             steps: An integer for forecast steps
+            include_history: Optional; A boolearn to specify whether to include historical data. Default is False.
 
         Returns:
             A pd.DataFrame that contains the forecast and confidence interval
@@ -186,23 +187,16 @@ class ARIMAModel(m.Model):
             "Call predict() with parameters. "
             "steps:{steps}, kwargs:{kwargs}".format(steps=steps, kwargs=kwargs)
         )
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `exog`.
+        self.include_history = include_history
         self.exog = kwargs.get("exog", None)
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `alpha`.
         self.alpha = kwargs.get("alpha", 0.05)
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `freq`.
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `data`.
         self.freq = kwargs.get("freq", pd.infer_freq(self.data.time))
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `model`.
         fcst = self.model.forecast(steps, exog=self.exog, alpha=self.alpha)
         logging.info("Generated forecast data from arima model.")
         logging.debug("Forecast data: {fcst}".format(fcst=fcst))
 
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `y_fcst`.
         self.y_fcst = fcst[0]
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `y_fcst_lower`.
         self.y_fcst_lower = np.array([x[0] for x in fcst[2]])
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `y_fcst_upper`.
         self.y_fcst_upper = np.array([x[1] for x in fcst[2]])
 
         last_date = self.data.time.max()
@@ -211,7 +205,6 @@ class ARIMAModel(m.Model):
         # pyre-fixme[16]: `ARIMAModel` has no attribute `dates`.
         self.dates = dates[dates != last_date]  # Return correct number of periods
 
-        # pyre-fixme[16]: `ARIMAModel` has no attribute `fcst_df`.
         self.fcst_df = pd.DataFrame(
             {
                 "time": self.dates,
@@ -220,7 +213,18 @@ class ARIMAModel(m.Model):
                 "fcst_upper": self.y_fcst_upper,
             }
         )
-
+        if self.include_history:
+            try:
+                hist_fcst = (
+                    self.model.predict(self.params.d, len(self.data))
+                    .reset_index()
+                    .rename(columns={"index": "time", 0: "fcst"})
+                )
+                self.fcst_df = pd.concat([hist_fcst, self.fcst_df])
+            except Exception as e:
+                msg = f"Fail to generate in-sample forecasts for historical data with error message {e}."
+                logging.error(msg)
+                raise ValueError(msg)
         logging.debug("Return forecast data: {fcst_df}".format(fcst_df=self.fcst_df))
         return self.fcst_df
 
