c966a410dbf648a3ca76353fe32faa9a8ea8178d
Emre Antonio Yurtbay <emreyurtbay@fb.com>
2021-06-14 14:12:04 -0700

    Add new TS features
    Summary: Adding some new TS features (Bollinger Bands [low, hi], RSI, TSI, TRIX, EMA) to feature_extraction.py
    
    Reviewed By: Papa-Panda
    
    Differential Revision: D29032908
    
    fbshipit-source-id: 9cf9286d8cc4803a40fb0a3e5f4816c1563499bb

--- kats/models/nowcasting/feature_extraction.py
+++ kats/models/nowcasting/feature_extraction.py
@@ -240,3 +240,146 @@ def MACD(df: pd.DataFrame, n_fast: int =12, n_slow: int =21, column: str = 'y') 
             nowcasting_features[6] = np.nan_to_num(np.nanmean(MACDdiff), nan=0.0, posinf=0.0, neginf=0.0)
 
         return nowcasting_features
+
+def BBANDS(df, n: int, column: str = 'y') -> pd.DataFrame:
+    '''Adds two Bolllinger Band columns
+
+    A Bollinger Band is a technical analysis tool defined by a
+    set of trendlines plotted two standard deviations (positively and negatively)
+    away from a simple moving average (SMA) of a security's price, but which can
+    be adjusted to user preferences.
+
+    Args:
+        df: a pandas dataframe
+        n: an integer on how many steps looking back.
+
+        column: Optional. If column is provided, will calculate based on provided column
+            otherwise the column named y will be the target.
+
+    Returns:
+        A dataframe with all the columns from input df, and the added 2 BollingerBand columns.
+    '''
+    close = df[column]
+    MA = pd.Series(close.rolling(n).mean())
+    MSD = pd.Series(close.rolling(n).std())
+    b1 = 4 * MSD / MA
+    B1 = pd.Series(b1, name = 'BollingerBand1_' + str(n))
+    df = df.join(B1)
+    b2 = (close - MA + 2 * MSD) / (4 * MSD)
+    B2 = pd.Series(b2, name = 'BollingerBand2_' + str(n))
+    df = df.join(B2)
+    return df
+
+def TRIX(df, n: int, column: str = 'y') -> pd.DataFrame:
+    '''Adds the TRIX indicator column
+
+    The triple exponential average (TRIX) indicator is an oscillator
+    used to identify oversold and overbought markets, and it can
+    also be used as a momentum indicator.
+
+    Args:
+        df: a pandas dataframe
+        n: an integer on how many steps looking back.
+
+        column: Optional. If column is provided, will calculate based on provided column
+            otherwise the column named y will be the target.
+
+    Returns:
+        A dataframe with all the columns from input df, and the added TRIX column.
+    '''
+    close = df[column]
+    EX1 = close.ewm( span = n, min_periods = n - 1).mean()
+    EX2 = EX1.ewm( span = n, min_periods = n - 1).mean()
+    EX3 = EX2.ewm( span = n, min_periods = n - 1).mean()
+
+    i = 0
+    ROC_l = [0]
+    while i + 1 <= df.index[-1]:
+        ROC = (EX3[i + 1] - EX3[i]) / EX3[i]
+        ROC_l.append(ROC)
+        i = i + 1
+    Trix = pd.Series(ROC_l, name = 'TRIX_' + str(n))
+    df = df.join(Trix)
+    return df
+
+def EMA(df, n: int, column: str = 'y') -> pd.DataFrame:
+    '''Adds the Exponetial Moving Average column
+
+    The exponential moving average (EMA) is a technical chart indicator that
+    tracks the price of an investment (like a stock or commodity) over time.
+    The EMA is a type of weighted moving average (WMA) that gives more
+     weighting or importance to recent price data
+
+    Args:
+        df: a pandas dataframe
+        n: an integer on how many steps looking back.
+
+        column: Optional. If column is provided, will calculate based on provided column
+            otherwise the column named y will be the target.
+
+    Returns:
+        A dataframe with all the columns from input df, and the added EMA column.
+    '''
+    close = df[column]
+    EMA = pd.Series(close.ewm( span = n, min_periods = n - 1).mean(), name = 'EMA_' + str(n))
+    df = df.join(EMA)
+    return df
+
+def TSI(df, r: int, s: int, column: str = 'y') -> pd.DataFrame:
+    '''Adds the TSI column
+
+    The true strength index (TSI) is a technical momentum oscillator
+    used to identify trends and reversals
+
+    Args:
+        df: a pandas dataframe
+        r: an integer on how many steps looking back, for window 1.
+        s: an integer on how many steps looking back, for window 2.
+
+        column: Optional. If column is provided, will calculate based on provided column
+            otherwise the column named y will be the target.
+
+    Returns:
+        A dataframe with all the columns from input df, and the added TSI column.
+    '''
+    close = df[column]
+    M = pd.Series(close.diff(1))
+    aM = abs(M)
+    EMA1 = pd.Series(M.ewm( span = r, min_periods = r - 1).mean())
+    aEMA1 = pd.Series(aM.ewm(span = r, min_periods = r - 1).mean())
+    EMA2 = pd.Series(EMA1.ewm( span = s, min_periods = s - 1).mean())
+    aEMA2 = pd.Series(aEMA1.ewm( span = s, min_periods = s - 1).mean())
+    TSI = pd.Series(EMA2 / aEMA2, name = 'TSI_' + str(r) + '_' + str(s))
+    df = df.join(TSI)
+    return df
+
+def RSI(df, n: int, column: str = 'y') -> pd.DataFrame:
+    '''
+    Relative Strength Index (RSI)
+    Compares the magnitude of recent gains and losses over a specified time
+    period to measure speed and change of price movements of a security. It is
+    primarily used to attempt to identify overbought or oversold conditions in
+    the trading of an asset.
+
+    Args:
+        df: a pandas dataframe
+        n: an integer on how many steps looking back.
+
+        column: Optional. If column is provided, will calculate based on provided column
+            otherwise the column named y will be the target.
+
+    Returns:
+        A dataframe with all the columns from input df, and the added RSI column.
+
+    '''
+    close = df[column]
+    diff = close.diff(1)
+    up_direction = diff.where(diff > 0, 0.0)
+    down_direction = -diff.where(diff < 0, 0.0)
+    min_periods = n
+    emaup = up_direction.ewm(alpha=1 / n, min_periods=min_periods, adjust=False).mean()
+    emadn = down_direction.ewm(alpha=1 / n, min_periods=min_periods, adjust=False).mean()
+    relative_strength = emaup / emadn
+    rsi_col = pd.Series(np.where(emadn == 0, 100, 100 - (100 / (1 + relative_strength))), name = 'RSI_' + str(n))
+    df = df.join(rsi_col)
+    return df
