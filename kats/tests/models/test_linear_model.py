1d36ca155475ae3dc0b44599c740744268166d85
Rohan Bopardikar <rohanfb@fb.com>
2021-09-01 17:01:19 -0700

    Make Linear Model Test More Robust
    Summary:
    This diff will serve as a template for making the model tests more robust.
    
    Note the following:
    1) Explicit testing of model correctness.
    2) Testing of multiple input types/variations.
    3) Parametrization.
    4) Dividing up unit tests into smaller pieces to clearly test individual components of functionality and improve efficiency by taking advantage of `multiprocessing`
    5) No real-time generation of input data.
    
    Reviewed By: iamxiaodong
    
    Differential Revision: D30634346
    
    fbshipit-source-id: f5ebe016d2a69eb278f53a11460645a92b4f9cac

--- kats/tests/models/test_linear_model.py
+++ kats/tests/models/test_linear_model.py
@@ -11,66 +11,157 @@ from unittest import TestCase
 import pandas as pd
 from kats.consts import TimeSeriesData
 from kats.models.linear_model import LinearModel, LinearModelParams
+from kats.tests.models.test_models_dummy_data import (
+    AIR_FCST_LINEAR_95,
+    AIR_FCST_LINEAR_99,
+    PEYTON_FCST_LINEAR_95,
+    PEYTON_FCST_LINEAR_99,
+    PEYTON_FCST_LINEAR_INVALID_NEG_ONE,
+    PEYTON_FCST_LINEAR_INVALID_ZERO,
+    PEYTON_FCST_LINEAR_NAN,
+    PEYTON_INPUT_NAN,
+)
+from pandas.util.testing import assert_frame_equal
+from parameterized import parameterized
 
 
-def load_data(file_name):
+def load_data(file_name, reset_columns=False):
     ROOT = "kats"
     if "kats" in os.getcwd().lower():
         path = "data/"
     else:
         path = "kats/data/"
     data_object = pkgutil.get_data(ROOT, path + file_name)
-    return pd.read_csv(io.BytesIO(data_object), encoding="utf8")
+    df = pd.read_csv(io.BytesIO(data_object), encoding="utf8")
+    if reset_columns:
+        df.columns = ["time", "y"]
+    return df
+
+
+TEST_DATA = {
+    "daily": {
+        "ts": TimeSeriesData(load_data("peyton_manning.csv", reset_columns=True)),
+        "ts_nan": TimeSeriesData(df=PEYTON_INPUT_NAN),
+        "freq": "D",
+        "res_95": PEYTON_FCST_LINEAR_95,
+        "res_99": PEYTON_FCST_LINEAR_99,
+        "res_invalid": PEYTON_FCST_LINEAR_NAN,
+        "res_invalid_param_1": PEYTON_FCST_LINEAR_INVALID_ZERO,
+        "res_invalid_param_2": PEYTON_FCST_LINEAR_INVALID_NEG_ONE,
+        "invalid_param_1": 0,
+        "invalid_param_2": -1,
+    },
+    "monthly": {
+        "ts": TimeSeriesData(load_data("air_passengers.csv", reset_columns=True)),
+        "freq": "MS",
+        "res_95": AIR_FCST_LINEAR_95,
+        "res_99": AIR_FCST_LINEAR_99,
+    },
+    "multi": {
+        "ts": TimeSeriesData(load_data("multivariate_anomaly_simulated_data.csv")),
+    },
+}
 
 
 class LinearModelTest(TestCase):
-    def setUp(self):
-        DATA = load_data("air_passengers.csv")
-        DATA.columns = ["time", "y"]
-        self.TSData = TimeSeriesData(DATA)
+    # pyre-fixme[16]: Module `parameterized.parameterized` has no attribute `expand`.
+    @parameterized.expand(
+        [
+            [
+                "monthly",
+                TEST_DATA["monthly"]["ts"],
+                TEST_DATA["monthly"]["freq"],
+                TEST_DATA["monthly"]["res_95"],
+                TEST_DATA["monthly"]["res_99"],
+            ],
+            [
+                "daily",
+                TEST_DATA["daily"]["ts"],
+                TEST_DATA["daily"]["freq"],
+                TEST_DATA["daily"]["res_95"],
+                TEST_DATA["daily"]["res_99"],
+            ],
+            [
+                "invalid",
+                TEST_DATA["daily"]["ts_nan"],
+                TEST_DATA["daily"]["freq"],
+                TEST_DATA["daily"]["res_invalid"],
+                TEST_DATA["daily"]["res_invalid"],
+            ],
+        ]
+    )
+    def test_fcst(
+        self,
+        name: str,
+        ts: TimeSeriesData,
+        freq: str,
+        truth_95: pd.DataFrame,
+        truth_99: pd.DataFrame,
+    ):
+        # Set up params
+        params_95 = LinearModelParams(alpha=0.05)
+        params_99 = LinearModelParams(alpha=0.01)
+        params_95.validate_params()
+        params_99.validate_params()
+        # Fit forecast
+        m_95 = LinearModel(ts, params_95)
+        m_99 = LinearModel(ts, params_99)
+        m_95.fit()
+        m_99.fit()
+        res_95 = m_95.predict(steps=30, freq=freq)
+        res_99 = m_99.predict(steps=30, freq=freq)
+        # Test result
+        assert_frame_equal(truth_95, res_95)
+        assert_frame_equal(truth_99, res_99)
 
-        DATA_daily = load_data("peyton_manning.csv")
-        DATA_daily.columns = ["time", "y"]
-        self.TSData_daily = TimeSeriesData(DATA_daily)
-
-        DATA_multi = load_data("multivariate_anomaly_simulated_data.csv")
-        self.TSData_multi = TimeSeriesData(DATA_multi)
-
-    def test_fit_forecast(self) -> None:
-        params = LinearModelParams(alpha=0.05)
-        params.validate_params()
-        m = LinearModel(self.TSData, params)
+    # pyre-fixme[16]: Module `parameterized.parameterized` has no attribute `expand`.
+    @parameterized.expand(
+        [
+            [
+                TEST_DATA["daily"]["ts"],
+                TEST_DATA["daily"]["invalid_param_1"],
+                TEST_DATA["daily"]["freq"],
+                TEST_DATA["daily"]["res_invalid_param_1"],
+            ],
+            [
+                TEST_DATA["daily"]["ts"],
+                TEST_DATA["daily"]["invalid_param_2"],
+                TEST_DATA["daily"]["freq"],
+                TEST_DATA["daily"]["res_invalid_param_2"],
+            ],
+        ]
+    )
+    def test_invalid_params(
+        self, ts: TimeSeriesData, invalid_param: float, freq: str, truth: pd.DataFrame
+    ):
+        # Set up params
+        params = LinearModelParams(alpha=invalid_param)
+        # Fit forecast
+        m = LinearModel(ts, params)
         m.fit()
-        m.predict(steps=30, freq="MS")
+        res = m.predict(steps=30, freq=freq, include_history=True)
+        # Test result
+        assert_frame_equal(truth, res)
+
+    def test_multivar(self):
+        self.assertRaises(ValueError, LinearModel, TEST_DATA["multi"]["ts"], None)
+
+    def test_exec_plot(self):
+        # Set up params
+        params = LinearModelParams(alpha=0.05)
+        # Fit forecast
+        m = LinearModel(TEST_DATA["daily"]["ts"], params)
+        m.fit()
+        _ = m.predict(steps=2, freq=TEST_DATA["daily"]["freq"])
+        # Test plotting
         m.plot()
 
-        m.predict(steps=30, freq="MS", include_history=True)
-        m.plot()
-
-        m_daily = LinearModel(self.TSData_daily, params)
-        m_daily.fit()
-        m_daily.predict(steps=30, freq="D")
-        m.plot()
-
-        m_daily.predict(steps=30, freq="D", include_history=True)
-        m.plot()
-
-    def test_others(self) -> None:
-        params = LinearModelParams()
-        params.validate_params()
-        self.assertRaises(
-            ValueError,
-            LinearModel,
-            self.TSData_multi,
-            params,
-        )
-
-        m = LinearModel(self.TSData, params)
-
-        # test __str__ method
+    def test_name(self):
+        m = LinearModel(TEST_DATA["daily"]["ts"], None)
         self.assertEqual(m.__str__(), "Linear Model")
 
-        # test search space
+    def test_search_space(self):
+        m = LinearModel(TEST_DATA["daily"]["ts"], None)
         self.assertEqual(
             m.get_parameter_search_space(),
             [
