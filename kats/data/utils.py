8a1de1aa4461ced8116b9307895e4e385f26b72a
Jeffrey Handler <jeffhandl@fb.com>
2021-09-15 18:19:04 -0700

    Implemented fit_predict for MKDetector
    Summary: Wrote `MKDetectorModel` to implement `fit_predict` so we can integrate `MKDetector` with OneDetection.
    
    Differential Revision: D30650599
    
    fbshipit-source-id: 8585f68dd7773dde35d941cc52d0cfcf1842ac7c

--- kats/data/utils.py
+++ kats/data/utils.py
@@ -21,8 +21,10 @@ import io
 import os
 import pkgutil
 
+import numpy as np
 import pandas as pd
 from kats.consts import TimeSeriesData
+from scipy.special import expit  # @manual
 
 
 def load_data(file_name):
@@ -79,3 +81,45 @@ def load_air_passengers(return_ts=True):
         return TimeSeriesData(df)
     else:
         return df
+
+
+# TODO: import this from test_trend_mk.py
+def gen_no_trend_data_ndim(time: pd.Series, ndim: int = 1):
+    n_days = len(time)
+    data = np.ones((n_days, ndim)) * np.random.randint(1000, size=(1, ndim))
+    no_trend_data = pd.DataFrame(data)
+    no_trend_data["time"] = time
+
+    return TimeSeriesData(no_trend_data)
+
+
+def gen_trend_data_ndim(
+    time: pd.Series,
+    seasonality: float = 0.00,
+    change_smoothness: float = 5.0,
+    ndim: int = 1,
+):
+    np.random.seed(20)
+
+    n_days = len(time)
+    ix = np.array([np.arange(n_days) for i in range(ndim)])
+    initial = np.random.randint(9000.0, 10000.0, size=(ndim, 1))
+    trend_change = -np.random.randint(60, size=(ndim, 1))
+    trend = np.random.randint(2.0, 6.0, size=(ndim, 1))
+    noise = np.array([1e-3] * ndim).reshape((ndim, 1))
+    t_change = np.random.randint(int(0.4 * n_days), int(0.7 * n_days), size=(ndim, 1))
+
+    data = (
+        (initial + trend * ix + trend_change * (ix - t_change) * expit((ix - t_change)))
+        * (1 - seasonality * (ix % 7 >= 5))
+        * np.array(
+            [np.cumprod(1 + noise[i] * np.random.randn(n_days)) for i in range(ndim)]
+        )
+    )
+
+    trend_data = pd.DataFrame(data.T)
+    trend_data["time"] = time
+
+    t_change = [t_change[i][0] for i in range(len(t_change))]
+
+    return TimeSeriesData(trend_data), t_change
