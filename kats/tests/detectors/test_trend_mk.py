75fa29ab4df0977c6e0299b3abe0d4198bfab537
Jeffrey Handler <jeffhandl@fb.com>
2021-09-27 13:01:41 -0700

    Cleaned up test functions from MKDetector
    Reviewed By: michaelbrundage
    
    Differential Revision: D31209669
    
    fbshipit-source-id: cc3b85a6e1b70785c90488e07e0b26322d465d42

--- kats/tests/detectors/test_trend_mk.py
+++ kats/tests/detectors/test_trend_mk.py
@@ -2,75 +2,19 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import io
-import os
-import pkgutil
 import re
 from operator import attrgetter
 from unittest import TestCase
 
-import numpy as np
 import pandas as pd
 import statsmodels
-from kats.consts import TimeSeriesData
 from kats.detectors.trend_mk import MKDetector
+from kats.tests.detectors.utils import gen_no_trend_data_ndim, gen_trend_data_ndim
 from parameterized import parameterized
-from scipy.special import expit  # @manual
 
 statsmodels_ver = float(
     re.findall("([0-9]+\\.[0-9]+)\\..*", statsmodels.__version__)[0]
 )
-
-
-def load_data(file_name):
-    ROOT = "kats"
-    if "kats" in os.getcwd().lower():
-        path = "data/"
-    else:
-        path = "kats/data/"
-    data_object = pkgutil.get_data(ROOT, path + file_name)
-    return pd.read_csv(io.BytesIO(data_object), encoding="utf8")
-
-
-def gen_no_trend_data_ndim(time: pd.Series, ndim: int = 1):
-    n_days = len(time)
-    data = np.ones((n_days, ndim)) * np.random.randint(1000, size=(1, ndim))
-    no_trend_data = pd.DataFrame(data)
-    no_trend_data["time"] = time
-
-    return TimeSeriesData(no_trend_data)
-
-
-def gen_trend_data_ndim(
-    time: pd.Series,
-    seasonality: float = 0.00,
-    change_smoothness: float = 5.0,
-    ndim: int = 1,
-):
-    np.random.seed(20)
-
-    n_days = len(time)
-    ix = np.array([np.arange(n_days) for i in range(ndim)])
-    initial = np.random.randint(9000.0, 10000.0, size=(ndim, 1))
-    trend_change = -np.random.randint(60, size=(ndim, 1))
-    trend = np.random.randint(2.0, 6.0, size=(ndim, 1))
-    noise = np.array([1e-3] * ndim).reshape((ndim, 1))
-    t_change = np.random.randint(int(0.4 * n_days), int(0.7 * n_days), size=(ndim, 1))
-
-    data = (
-        (initial + trend * ix + trend_change * (ix - t_change) * expit((ix - t_change)))
-        * (1 - seasonality * (ix % 7 >= 5))
-        * np.array(
-            [np.cumprod(1 + noise[i] * np.random.randn(n_days)) for i in range(ndim)]
-        )
-    )
-
-    trend_data = pd.DataFrame(data.T)
-    trend_data["time"] = time
-
-    t_change = [t_change[i][0] for i in range(len(t_change))]
-
-    return TimeSeriesData(trend_data), t_change
 
 
 class UnivariateMKDetectorTest(TestCase):
---
+++ kats/tests/detectors/utils.py
@@ -0,0 +1,64 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Base I/O code for time series data in Kats
+
+This is a base implementation to load datasets for test and evaluation/benchmarking
+purposes. We currently support the following data sets:
+
+1. air passengers data
+2. m3 meta data
+3. Peyton manning data
+4. retail sales data
+5. yosemite temps data
+6. multi ts data
+7. mean change detection test data
+8. multivariate anomaly simulated data
+"""
+
+import numpy as np
+import pandas as pd
+from kats.consts import TimeSeriesData
+from scipy.special import expit  # @manual
+
+
+def gen_no_trend_data_ndim(time: pd.Series, ndim: int = 1):
+    n_days = len(time)
+    data = np.ones((n_days, ndim)) * np.random.randint(1000, size=(1, ndim))
+    no_trend_data = pd.DataFrame(data)
+    no_trend_data["time"] = time
+
+    return TimeSeriesData(no_trend_data)
+
+
+def gen_trend_data_ndim(
+    time: pd.Series,
+    seasonality: float = 0.00,
+    change_smoothness: float = 5.0,
+    ndim: int = 1,
+):
+    np.random.seed(20)
+
+    n_days = len(time)
+    ix = np.array([np.arange(n_days) for i in range(ndim)])
+    initial = np.random.randint(9000.0, 10000.0, size=(ndim, 1))
+    trend_change = -np.random.randint(60, size=(ndim, 1))
+    trend = np.random.randint(2.0, 6.0, size=(ndim, 1))
+    noise = np.array([1e-3] * ndim).reshape((ndim, 1))
+    t_change = np.random.randint(int(0.4 * n_days), int(0.7 * n_days), size=(ndim, 1))
+
+    data = (
+        (initial + trend * ix + trend_change * (ix - t_change) * expit((ix - t_change)))
+        * (1 - seasonality * (ix % 7 >= 5))
+        * np.array(
+            [np.cumprod(1 + noise[i] * np.random.randn(n_days)) for i in range(ndim)]
+        )
+    )
+
+    trend_data = pd.DataFrame(data.T)
+    trend_data["time"] = time
+
+    t_change = [t_change[i][0] for i in range(len(t_change))]
+
+    return TimeSeriesData(trend_data), t_change
