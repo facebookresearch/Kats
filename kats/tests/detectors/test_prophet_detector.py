c9e50e23aeba570719f15136a1662b8c42e3119c
Aaron Schumacher <ajschumacher@fb.com>
2021-09-30 10:37:57 -0700

    parameterize tests for Prophet Detector
    Summary: Rewrites tests in `test_prophet_detector.py` so that they always have exactly one assertion, usually by using `parameterize.expand()` to replace loops. This takes the passing test count from 11 to 38.
    
    Reviewed By: jeffhandl
    
    Differential Revision: D31249486
    
    fbshipit-source-id: 622dd910692ed2df27b3f74451ff08ac774c8a58

--- kats/tests/detectors/test_prophet_detector.py
+++ kats/tests/detectors/test_prophet_detector.py
@@ -16,6 +16,7 @@ from kats.detectors.prophet_detector import (
     ProphetScoreFunction,
 )
 from kats.utils.simulator import Simulator
+from parameterized import parameterized
 
 statsmodels_ver = float(
     re.findall("([0-9]+\\.[0-9]+)\\..*", statsmodels.__version__)[0]
@@ -231,42 +232,58 @@ class TestProphetDetector(TestCase):
         else:
             return (actual_val - predicted_val) / lower_std
 
-    def test_no_anomaly(self) -> None:
+    def scenario_results(self, seed, include_anomaly, use_serialized_model):
+        """Prediction results for common data and model test scenarios"""
+        ts = self.create_random_ts(seed, 100, 10, 2)
+
+        if include_anomaly:
+            self.add_smooth_anomaly(ts, seed, 90, 10, 10)
+
+        model = ProphetDetectorModel()
+        model.fit(ts[:90])
+
+        if use_serialized_model:
+            serialized_model = model.serialize()
+            model = ProphetDetectorModel(serialized_model=serialized_model)
+
+        return model.predict(ts[90:])
+
+    # Alternate between using the current model and using serialized model
+    SEED_AND_SERIALIZATIONS = [[0, True], [1, False], [2, True], [3, False], [4, True]]
+
+    # pyre-ignore Undefined attribute [16]: Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(SEED_AND_SERIALIZATIONS)
+    def test_no_anomaly_prediction_length(self, seed, use_serialized_model) -> None:
+        include_anomaly = False
+        res = self.scenario_results(seed, include_anomaly, use_serialized_model)
+        self.assertEqual(len(res.scores), 10)
+
+    # pyre-ignore Undefined attribute [16]: Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(SEED_AND_SERIALIZATIONS)
+    def test_anomaly_prediction_length(self, seed, use_serialized_model) -> None:
+        include_anomaly = True
+        res = self.scenario_results(seed, include_anomaly, use_serialized_model)
+        self.assertEqual(len(res.scores), 10)
+
+    # pyre-ignore Undefined attribute [16]: Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(SEED_AND_SERIALIZATIONS)
+    def test_finds_no_anomaly_when_no_anomoly(self, seed, use_serialized_model) -> None:
         # Prophet should not find any anomalies on a well formed synthetic time series
-        for i in range(0, 5):
-            ts = self.create_random_ts(i, 100, 10, 2)
+        include_anomaly = False
+        res = self.scenario_results(seed, include_anomaly, use_serialized_model)
+        anomaly_found = res.scores.min < -0.3 or res.scores.max > 0.3
+        self.assertFalse(anomaly_found)
 
-            model = ProphetDetectorModel()
-            model.fit(ts[:90])
-
-            # alternate between using the current model and using serialized model
-            if i % 2 == 0:
-                serialized_model = model.serialize()
-                model = ProphetDetectorModel(serialized_model=serialized_model)
-
-            res = model.predict(ts[90:])
-            self.assertEqual(len(res.scores), 10)
-            anomaly_found = res.scores.min < -0.3 or res.scores.max > 0.3
-            self.assertFalse(anomaly_found)
-
-    def test_anomaly(self) -> None:
+    # pyre-ignore Undefined attribute [16]: Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(SEED_AND_SERIALIZATIONS)
+    def test_finds_anomaly_when_anomaly_present(
+        self, seed, use_serialized_model
+    ) -> None:
         # Prophet should find anomalies
-        for i in range(0, 5):
-            ts = self.create_random_ts(i, 100, 10, 2)
-            self.add_smooth_anomaly(ts, i, 90, 10, 10)
-
-            model = ProphetDetectorModel()
-            model.fit(ts[:90])
-
-            # alternate between using the current model and using serialized model
-            if i % 2 == 0:
-                serialized_model = model.serialize()
-                model = ProphetDetectorModel(serialized_model=serialized_model)
-
-            res = model.predict(ts[90:])
-            self.assertEqual(len(res.scores), 10)
-            anomaly_found = res.scores.min < -0.3 or res.scores.max > 0.3
-            self.assertTrue(anomaly_found)
+        include_anomaly = True
+        res = self.scenario_results(seed, include_anomaly, use_serialized_model)
+        anomaly_found = res.scores.min < -0.3 or res.scores.max > 0.3
+        self.assertTrue(anomaly_found)
 
     def test_fit_predict(self) -> None:
         ts = self.create_random_ts(0, 100, 10, 2)
@@ -281,23 +298,44 @@ class TestProphetDetector(TestCase):
 
         self.assertEqual(res0.scores.value.to_list(), res1.scores.value.to_list())
 
-    def test_outlier_removal_threshold(self):
+    @parameterized.expand([["moderate", 0.99], ["aggressive", 0.8]])
+    def test_outlier_removal_threshold(self, name, threshold):
         ts = self.create_random_ts(0, 365, 10, 2)
         ts_df = pd.DataFrame({"ds": ts.time, "y": ts.value})
 
         model = ProphetDetectorModel()
 
-        filtered_ts_df = model._remove_outliers(ts_df, outlier_ci_threshold=0.99)
-
-        aggressively_filtered_ts_df = model._remove_outliers(
-            ts_df, outlier_ci_threshold=0.8
-        )
+        filtered_ts_df = model._remove_outliers(ts_df, outlier_ci_threshold=threshold)
 
         self.assertGreaterEqual(len(ts_df), len(filtered_ts_df))
-        self.assertGreaterEqual(len(filtered_ts_df), len(aggressively_filtered_ts_df))
 
-    def test_outlier_removal_efficacy(self):
-        def _subtest(
+    @parameterized.expand(
+        [
+            ["early event", 0.1, 0.15, 0.3, 1.5],
+            ["late event", 0.1, 0.72, 0.85, -2],
+            ["spiky event", 0.1, 0.5, 0.55, 5],
+            ["prolonged event", 0.1, 0.35, 0.62, -1.5],
+            ["noisier data", 0.5, 0.5, 0.55, 5],
+        ]
+    )
+    def test_outlier_removal_efficacy(
+        self,
+        name,
+        signal_to_noise_ratio,
+        event_start_ratio,
+        event_end_ratio,
+        event_relative_magnitude,
+    ):
+        seed = 0
+        length = 960
+        freq = "15min"
+        min_val = 0
+        max_val = 1000
+
+        baseline_ts = self.create_multi_seasonality_ts(
+            seed, length, freq, min_val, max_val, signal_to_noise_ratio
+        )
+        test_ts = self.add_multi_event(
             baseline_ts,
             seed,
             length,
@@ -308,78 +346,43 @@ class TestProphetDetector(TestCase):
             event_start_ratio,
             event_end_ratio,
             event_relative_magnitude,
-        ):
-            model = ProphetDetectorModel()
+        )
 
-            test_ts = self.add_multi_event(
-                baseline_ts,
-                seed,
-                length,
-                freq,
-                min_val,
-                max_val,
-                signal_to_noise_ratio,
-                event_start_ratio,
-                event_end_ratio,
-                event_relative_magnitude,
-            )
+        model = ProphetDetectorModel()
 
-            # Train on all data up to 0.5 days after the event
-            event_end_idx = int(length * event_end_ratio)
-            train_idx = (
-                test_ts.time >= test_ts.time.iloc[event_end_idx] + timedelta(hours=12)
-            ).idxmax()
+        # Train on all data up to 0.5 days after the event
+        event_end_idx = int(length * event_end_ratio)
+        train_idx = (
+            test_ts.time >= test_ts.time.iloc[event_end_idx] + timedelta(hours=12)
+        ).idxmax()
 
-            test_df = test_ts.to_dataframe()
-            train_ts = TimeSeriesData(df=test_df.iloc[:train_idx])
-            pred_ts_df_map = {}
-            for remove_outliers in [False, True]:
-                model.remove_outliers = remove_outliers
-                # Test on all the remaining data
-                pred_ts_df_map[remove_outliers] = model.fit_predict(test_ts, train_ts)
+        test_df = test_ts.to_dataframe()
+        train_ts = TimeSeriesData(df=test_df.iloc[:train_idx])
+        pred_ts_df_map = {}
+        for remove_outliers in [False, True]:
+            model.remove_outliers = remove_outliers
+            # Test on all the remaining data
+            pred_ts_df_map[remove_outliers] = model.fit_predict(test_ts, train_ts)
 
-            # Model trained without outliers should have lower RMSE
-            rmse_w_outliers = (
-                (pred_ts_df_map[False].predicted_ts.value - test_ts.value) ** 2
-            ).mean() ** 0.5
-            rmse_no_outliers = (
-                (pred_ts_df_map[True].predicted_ts.value - test_ts.value) ** 2
-            ).mean() ** 0.5
-            self.assertGreaterEqual(
-                rmse_w_outliers,
-                rmse_no_outliers,
-                "Expected removing outliers when training model to lower prediciton RMSE",
-            )
+        # Model trained without outliers should have lower RMSE
+        rmse_w_outliers = (
+            (pred_ts_df_map[False].predicted_ts.value - test_ts.value) ** 2
+        ).mean() ** 0.5
+        rmse_no_outliers = (
+            (pred_ts_df_map[True].predicted_ts.value - test_ts.value) ** 2
+        ).mean() ** 0.5
+        self.assertGreaterEqual(
+            rmse_w_outliers,
+            rmse_no_outliers,
+            "Expected removing outliers when training model to lower prediction RMSE",
+        )
 
-        baseline_ts = self.create_multi_seasonality_ts(0, 960, "15min", 0, 1000, 0.1)
+    def test_default_score_func(self):
+        """Test that 'deviation_from_predicted_val' is used by default
 
-        with self.subTest("Testing with early event"):
-            _subtest(baseline_ts, 0, 960, "15min", 0, 1000, 0.1, 0.15, 0.3, 1.5)
-
-        with self.subTest("Testing with late event"):
-            _subtest(baseline_ts, 0, 960, "15min", 0, 1000, 0.1, 0.72, 0.85, -2)
-
-        with self.subTest("Testing with spiky event"):
-            _subtest(baseline_ts, 0, 960, "15min", 0, 1000, 0.1, 0.5, 0.55, 5)
-
-        with self.subTest("Testing with prolonged event"):
-            _subtest(baseline_ts, 0, 960, "15min", 0, 1000, 0.1, 0.35, 0.62, -1.5)
-
-        noisy_ts = self.create_multi_seasonality_ts(0, 960, "15min", 0, 1000, 0.5)
-
-        with self.subTest("Testing with noisy underlying data"):
-            _subtest(noisy_ts, 0, 960, "15min", 0, 1000, 0.5, 0.5, 0.55, 5)
-
-    def test_score_parameter(self):
-        """Tests the behavior of the score_func parameter.
-
-        This test verifies:
-        (1) the default implementation of ProphetDetectorModel
-        uses the 'deviation_from_predicted_val' scoring function;
-        (2) passing ProphetScoreFunction.z_score as the 'score_func' results in
-        ProphetDetectorModel implementing the 'z_score' scoring function;
-        (3) the anomaly scores returned by each of these functions
-        are identical to the actual deviation and actual z_score.
+        This test verifies that the default implementation of
+        ProphetDetectorModel uses the 'deviation_from_predicted_val' scoring
+        function, by checking an anomaly score.
         """
         ts = self.create_ts()
 
@@ -396,6 +399,18 @@ class TestProphetDetector(TestCase):
             ),
         )
 
+    def test_score_func_parameter_as_z_score(self):
+        """Test that score_func parameter can be set to z_score
+
+        This test verifies that passing ProphetScoreFunction.z_score as the
+        'score_func' results in ProphetDetectorModel implementing the
+        'z_score' scoring function, by checking an anomaly score.
+        """
+        ts = self.create_ts()
+
+        # add anomaly at index 95
+        ts.value[95] += 100
+
         z_score_model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
         z_score_response = z_score_model.fit_predict(ts[90:], ts[:90])
         actual_z_score = self.calc_z_score(
@@ -408,7 +423,8 @@ class TestProphetDetector(TestCase):
             z_score_response.scores.value[5], actual_z_score, places=15
         )
 
-    def test_flat_signal(self):
+    @parameterized.expand([["no anomaly", 0], ["with anomaly", 100]])
+    def test_flat_signal(self, name, anomaly_magnitude):
         """Tests the behavior of the z-score strategy on flat signals.
 
         This test verifies that the model's z_scores of flat signals
@@ -418,20 +434,20 @@ class TestProphetDetector(TestCase):
         """
         ts = self.create_ts(magnitude=0, signal_to_noise_ratio=0)
 
-        for anomaly_magnitude in (0, 100):
-            ts.value[95] += anomaly_magnitude
+        ts.value[95] += anomaly_magnitude
 
-            model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
-            response = model.fit_predict(ts[90:], ts[:90])
-            actual_z_score = self.calc_z_score(
-                ts.value[95],
-                response.predicted_ts.value[5],
-                response.confidence_band.upper.value[5],
-                response.confidence_band.lower.value[5],
-            )
-            self.assertAlmostEqual(response.scores.value[5], actual_z_score, places=15)
+        model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
+        response = model.fit_predict(ts[90:], ts[:90])
+        actual_z_score = self.calc_z_score(
+            ts.value[95],
+            response.predicted_ts.value[5],
+            response.confidence_band.upper.value[5],
+            response.confidence_band.lower.value[5],
+        )
+        self.assertAlmostEqual(response.scores.value[5], actual_z_score, places=15)
 
-    def test_zero_noise_signal(self):
+    @parameterized.expand([["no anomaly", 0], ["with anomaly", 100]])
+    def test_zero_noise_signal(self, name, anomaly_magnitude):
         """Tests the behavior of the z-score strategy on zero-noise signals.
 
         This test verifies that the model's z_scores of zero-noise signals
@@ -441,18 +457,17 @@ class TestProphetDetector(TestCase):
         """
         ts = self.create_ts(signal_to_noise_ratio=0)
 
-        for anomaly_magnitude in (0, 100):
-            ts.value[95] += anomaly_magnitude
+        ts.value[95] += anomaly_magnitude
 
-            model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
-            response = model.fit_predict(ts[90:], ts[:90])
-            actual_z_score = self.calc_z_score(
-                ts.value[95],
-                response.predicted_ts.value[5],
-                response.confidence_band.upper.value[5],
-                response.confidence_band.lower.value[5],
-            )
-            self.assertAlmostEqual(response.scores.value[5], actual_z_score, places=15)
+        model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
+        response = model.fit_predict(ts[90:], ts[:90])
+        actual_z_score = self.calc_z_score(
+            ts.value[95],
+            response.predicted_ts.value[5],
+            response.confidence_band.upper.value[5],
+            response.confidence_band.lower.value[5],
+        )
+        self.assertAlmostEqual(response.scores.value[5], actual_z_score, places=15)
 
     @unittest.skip(
         "Prophet doesn't learn heteroskedastic seasonality with params used by ProphetDetectorModel"
@@ -499,7 +514,8 @@ class TestProphetDetector(TestCase):
 
         self.assertGreater(response.scores.value[3 * 24], response.scores.value[6 * 24])
 
-    def test_asymmetric_noise_signal(self):
+    @parameterized.expand([[3 * 24], [6 * 24]])
+    def test_asymmetric_noise_signal(self, test_index):
         """Tests the z-score strategy on signals with asymmetric noise
 
         This test verifies that the asymmetric z-scores function as expected when
@@ -531,8 +547,5 @@ class TestProphetDetector(TestCase):
         response2 = model.fit_predict(test_ts[90 * 24 :], ts2[: 90 * 24])
 
         self.assertGreater(
-            response2.scores.value[3 * 24], response1.scores.value[3 * 24]
-        )
-        self.assertGreater(
-            response2.scores.value[6 * 24], response1.scores.value[6 * 24]
+            response2.scores.value[test_index], response1.scores.value[test_index]
         )
