d953e574ae385b34f5782b28bbaf4c5587441679
Michael Brundage <brundage@fb.com>
2021-10-13 20:27:59 -0700

    Fix Pyre errors in models/lstm.py
    Summary:
    Fix all but one Pyre error in `models/lstm.py`. The remaining one is because `predict()` signature doesn't match the superclass, which is a larger issue to address.
    
    This appears to have revealed an existing latent bug. `predict` was setting `hidden` instead of `hidden_cell` on the model.
    
    Also improve documentation formatting while I'm there.
    
    Reviewed By: rohanfb
    
    Differential Revision: D31621538
    
    fbshipit-source-id: 7be53a46bc744ec8da4d5cfaf3de6d090eecaf12

--- kats/models/lstm.py
+++ kats/models/lstm.py
@@ -2,16 +2,20 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""The LSTM model stands for Long short-term memory, it is a recurrent neural network model that can be used for sequential data.
+"""Apply LSTM for time series forecasting.
 
-More information for the model can be found: https://en.wikipedia.org/wiki/Long_short-term_memory
-We directly adopt the PyTorch implementation and apply the model for time series forecast. More details for the PyTorch modules are here: https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+The LSTM (long short-term memory) model is a recurrent neural network model that
+can be used for sequential data.
+
+See https://en.wikipedia.org/wiki/Long_short-term_memory for more info.
+
+We directly adopt the PyTorch implementation and apply the model for time series
+forecast. More details for the PyTorch modules are here:
+https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
 """
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import logging
-from typing import Tuple, List, Dict, Any
+from typing import Any, Dict, List, Optional, Tuple
 
 import kats.models.model as mm
 import numpy as np
@@ -25,7 +29,8 @@ from sklearn.preprocessing import MinMaxScaler
 class LSTMParams(Params):
     """Parameter class for time series LSTM model
 
-    This is the parameter class for time series LSTM model, it currently contains three parameters
+    This is the parameter class for time series LSTM model, it currently
+    contains three parameters.
 
     Attributes:
         hidden_size: LSTM hidden unit size
@@ -43,7 +48,8 @@ class LSTMParams(Params):
 
         logging.debug(
             "Initialized LSTMParams instance."
-            f"hidden_size:{hidden_size}, time_window:{time_window}, num_epochs:{num_epochs}"
+            f"hidden_size:{hidden_size}, time_window:{time_window}"
+            f" num_epochs:{num_epochs}"
         )
 
     def validate_params(self):
@@ -54,7 +60,9 @@ class LSTMParams(Params):
 class LSTMForecast(nn.Module):
     """Torch forecast class for time series LSTM model
 
-    This is the forecast class for time series LSTM model inherited from the PyTorch module, detailed implementation for the core LSTM and Linear modules can be gound here:
+    This is the forecast class for time series LSTM model inherited from the
+    PyTorch module, detailed implementation for the core LSTM and Linear modules
+    can be found here:
     https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
     https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
 
@@ -63,6 +71,8 @@ class LSTMForecast(nn.Module):
         input_size: Input unit feature size for the LSTM layer
         output_size: Output unit feature size from the output Linear layer
     """
+
+    hidden_cell: Optional[Tuple[torch.Tensor, torch.Tensor]] = None
 
     def __init__(self, params: LSTMParams, input_size: int, output_size: int) -> None:
         super().__init__()
@@ -77,13 +87,14 @@ class LSTMForecast(nn.Module):
         """The forward method for the LSTM forecast PyTorch module
 
         Args:
-            input_seq: A torch tensor contains the input data sequence for the LSTM layer
+            input_seq: A torch tensor contains the input data sequence for the
+            LSTM layer.
 
         Returns:
-            prediction: A torch tensor contains the output prediction from the output Linear layer
+            prediction: A torch tensor contains the output prediction from the
+            output Linear layer
         """
 
-        # pyre-fixme[16]: `LSTMForecast` has no attribute `hidden_cell`.
         lstm_out, self.hidden_cell = self.lstm(
             input_seq.view(len(input_seq), 1, -1), self.hidden_cell
         )
@@ -101,6 +112,17 @@ class LSTMModel(mm.Model):
         params: A LSTMParams object for the parameters
     """
 
+    train_data_normalized: Optional[torch.FloatTensor] = None
+    scaler: Optional[MinMaxScaler] = None
+    lr: Optional[float] = None
+    model: Optional[LSTMForecast] = None
+    freq: Optional[str] = None
+    dates: Optional[pd.DatetimeIndex] = None
+    y_fcst: Optional[np.ndarray] = None
+    y_fcst_lower: Optional[np.ndarray] = None
+    y_fcst_upper: Optional[np.ndarray] = None
+    fcst_df: Optional[pd.DataFrame] = None
+
     def __init__(self, data: TimeSeriesData, params: LSTMParams) -> None:
         super().__init__(data, params)
         if not isinstance(self.data.value, pd.Series):
@@ -113,40 +135,40 @@ class LSTMModel(mm.Model):
     def __setup_data(self) -> List[Tuple[torch.Tensor, torch.Tensor]]:
         """Prepare input data for the LSTM model
 
-        This method will perform a min-max normalization on the input data, then output normalized input sequence data and true values for the prediction. More details for the normalizer:
+        This method will perform a min-max normalization on the input data, then
+        output normalized input sequence data and true values for the prediction.
+        More details for the normalizer:
         https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html
 
         Args:
             None
 
         Returns:
-            A list of tuples that include both the input sequence tensor (with normalized values) and ground truth value for prediction
+            A list of tuples that include both the input sequence tensor (with
+            normalized values) and ground truth value for prediction.
         """
 
         train_data = self.data.value.values.astype(float)
 
         # scaling using MinMaxScaler
-        # pyre-fixme[16]: `LSTMModel` has no attribute `scaler`.
-        # pyre-fixme[16]: Module `sklearn` has no attribute `preprocessing`.
         self.scaler = MinMaxScaler(feature_range=(-1, 1))
         train_data_scaled = self.scaler.fit_transform(train_data.reshape(-1, 1))
         # converting to Tensor
-        # pyre-fixme[16]: `LSTMModel` has no attribute `train_data_normalized`.
-        self.train_data_normalized = torch.FloatTensor(train_data_scaled).view(-1)
+        train_data_normalized = torch.FloatTensor(train_data_scaled).view(-1)
+        self.train_data_normalized = train_data_normalized
 
         # generating sequence
         inout_seq = []
 
-        for i in range(len(self.train_data_normalized) - self.params.time_window):
-            train_seq = self.train_data_normalized[i : i + self.params.time_window]
-            train_label = self.train_data_normalized[
-                i + self.params.time_window : i + self.params.time_window + 1
-            ]
+        time_window = self.params.time_window
+        for i in range(len(train_data_normalized) - time_window):
+            train_seq = train_data_normalized[i : i + time_window]
+            train_label = train_data_normalized[i + time_window : i + time_window + 1]
             inout_seq.append((train_seq, train_label))
 
         return inout_seq
 
-    def fit(self, **kwargs) -> None:
+    def fit(self, **kwargs) -> "LSTMModel":
         """Fit the LSTM forecast model
 
         Args:
@@ -159,19 +181,17 @@ class LSTMModel(mm.Model):
         logging.debug("Call fit() with parameters." f"kwargs:{kwargs}")
 
         # learning rate
-        # pyre-fixme[16]: `LSTMModel` has no attribute `lr`.
         self.lr = kwargs.get("lr", 0.001)
 
         # supports univariate time series, multivariate support in the future
-        # pyre-fixme[16]: `LSTMModel` has no attribute `model`.
-        # pyre-fixme[16]: `LSTMModel` has no attribute `params`.
-        self.model = LSTMForecast(params=self.params, input_size=1, output_size=1)
+        model = LSTMForecast(params=self.params, input_size=1, output_size=1)
+        self.model = model
 
         # loss function
         loss_function = nn.MSELoss()
 
         # optimizer
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
 
         # inout_seq
         train_inout_seq = self.__setup_data()
@@ -179,13 +199,12 @@ class LSTMModel(mm.Model):
         for i in range(self.params.num_epochs):
             for seq, labels in train_inout_seq:
                 optimizer.zero_grad()
-                # pyre-fixme[16]: `LSTMForecast` has no attribute `hidden_cell`.
-                self.model.hidden_cell = (
+                model.hidden_cell = (
                     torch.zeros(1, 1, self.params.hidden_size),
                     torch.zeros(1, 1, self.params.hidden_size),
                 )
                 # prediction using input data
-                y_pred = self.model(seq)
+                y_pred = model(seq)
                 # calculating loss
                 single_loss = loss_function(y_pred, labels)
                 single_loss.backward()
@@ -194,7 +213,6 @@ class LSTMModel(mm.Model):
             if i % 25 == 1:
                 logging.info(f"epoch: {i:3} loss: {single_loss.item():10.8f}")
 
-        # pyre-fixme[7]: Expected `None` but got `LSTMModel`.
         return self
 
     # pyre-fixme[14]: `predict` overrides method defined in `Model` inconsistently.
@@ -207,52 +225,49 @@ class LSTMModel(mm.Model):
         Returns:
             A pd.DataFrame that includes the forecast and confidence interval
         """
+        model = self.model
+        if model is None:
+            raise ValueError("Call fit() before predict()")
+        train_data_normalized = self.train_data_normalized
+        scaler = self.scaler
+        assert train_data_normalized is not None and scaler is not None
+        time_window = self.params.time_window
+        hidden_size = self.params.hidden_size
 
         logging.debug(
             "Call predict() with parameters. " f"steps:{steps}, kwargs:{kwargs}"
         )
-        # pyre-fixme[16]: `LSTMModel` has no attribute `freq`.
-        # pyre-fixme[16]: `LSTMModel` has no attribute `data`.
         self.freq = kwargs.get("freq", pd.infer_freq(self.data.time))
 
-        # pyre-fixme[16]: `LSTMModel` has no attribute `model`.
-        self.model.eval()
+        model.eval()
 
         # get last train input sequence
-        # pyre-fixme[16]: `LSTMModel` has no attribute `train_data_normalized`.
-        # pyre-fixme[16]: `LSTMModel` has no attribute `params`.
-        test_inputs = self.train_data_normalized[-self.params.time_window :].tolist()
+        test_inputs = train_data_normalized[-time_window:].tolist()
 
         for _ in range(steps):
-            seq = torch.FloatTensor(test_inputs[-self.params.time_window :])
+            seq = torch.FloatTensor(test_inputs[-time_window:])
             with torch.no_grad():
-                self.model.hidden = (
-                    torch.zeros(1, 1, self.params.hidden_size),
-                    torch.zeros(1, 1, self.params.hidden_size),
+                model.hidden_cell = (
+                    torch.zeros(1, 1, hidden_size),
+                    torch.zeros(1, 1, hidden_size),
                 )
-                test_inputs.append(self.model(seq).item())
+                test_inputs.append(model(seq).item())
 
         # inverse transform
-        # pyre-fixme[16]: `LSTMModel` has no attribute `scaler`.
-        fcst_denormalized = self.scaler.inverse_transform(
-            np.array(test_inputs[self.params.time_window :]).reshape(-1, 1)
+        fcst_denormalized = scaler.inverse_transform(
+            np.array(test_inputs[time_window:]).reshape(-1, 1)
         ).flatten()
         logging.info("Generated forecast data from LSTM model.")
         logging.debug(f"Forecast data: {fcst_denormalized}")
 
         last_date = self.data.time.max()
         dates = pd.date_range(start=last_date, periods=steps + 1, freq=self.freq)
-        # pyre-fixme[16]: `LSTMModel` has no attribute `dates`.
         self.dates = dates[dates != last_date]  # Return correct number of periods
-        # pyre-fixme[16]: `LSTMModel` has no attribute `y_fcst`.
         self.y_fcst = fcst_denormalized
-        # pyre-fixme[16]: `LSTMModel` has no attribute `y_fcst_lower`.
         self.y_fcst_lower = fcst_denormalized * 0.95
-        # pyre-fixme[16]: `LSTMModel` has no attribute `y_fcst_upper`.
         self.y_fcst_upper = fcst_denormalized * 1.05
 
-        # pyre-fixme[16]: `LSTMModel` has no attribute `fcst_df`.
-        self.fcst_df = pd.DataFrame(
+        self.fcst_df = fcst_df = pd.DataFrame(
             {
                 "time": self.dates,
                 "fcst": self.y_fcst,
@@ -263,7 +278,7 @@ class LSTMModel(mm.Model):
 
         logging.debug(f"Return forecast data: {self.fcst_df}")
 
-        return self.fcst_df
+        return fcst_df
 
     def plot(self):
         """Plot forecast results from the LSTM model"""
