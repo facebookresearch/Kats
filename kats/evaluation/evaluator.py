3c7dda7f2c29e8068e110f3e7d52bb56f2b6a8a5
Rohan Bopardikar <rohanfb@fb.com>
2021-10-15 14:16:23 -0700

    Generic Evaluation Framework
    Summary: See https://fb.quip.com/7WRbAyvwMgG0#cYKAAAj49kd
    
    Reviewed By: michaelbrundage
    
    Differential Revision: D30889775
    
    fbshipit-source-id: 5a6cae5d1b0524ab36a95accd9dbda8cda42d91a

---
+++ kats/evaluation/evaluator.py
@@ -0,0 +1,103 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Dict, Type, Union, Optional, Sequence, Callable
+
+import numpy as np
+import pandas as pd
+from kats.consts import Params
+from kats.consts import _log_error, TimeSeriesData
+from kats.detectors.detector import DetectorModel
+from kats.models.model import Model
+
+ArrayLike = Union[np.ndarray, Sequence[float], pd.Series]
+
+
+@dataclass
+class EvaluationObject:
+    input_data: Optional[Union[ArrayLike, TimeSeriesData, pd.DataFrame]]
+    model: Optional[Union[Model, DetectorModel]]
+    preds: Optional[ArrayLike]
+    labels: Optional[ArrayLike]
+    results: Optional[pd.DataFrame]
+
+
+class Evaluator(ABC):
+    def __init__(self):
+        self.runs: Dict[str, EvaluationObject] = {}
+
+    def create_evaluation_run(self, run_name: str) -> None:
+        self._check_if_valid_run_name(run_name=run_name, should_exist=False)
+        self.runs[run_name] = EvaluationObject(None, None, None, None, None)
+
+    def delete_evaluation_run(self, run_name: str):
+        self._check_if_valid_run_name(run_name=run_name)
+        del self.runs[run_name]
+
+    def _check_if_valid_run_name(
+        self, run_name: str, should_exist: bool = True
+    ) -> None:
+        if not isinstance(run_name, str):  # Check if name is a string
+            msg = f"Name of evaluation run must be a string, but is of type {type(run_name)}."
+            raise _log_error(msg)
+        # Handle case depending on if run_name should exist or not
+        if should_exist != (run_name in self.runs):
+            msg = f"Run name {run_name} {'already exists' if run_name in self.runs else 'does not exist'}. Please choose a valid run name."
+            raise _log_error(msg)
+
+    def get_evaluation_run(self, run_name: str) -> EvaluationObject:
+        self._check_if_valid_run_name(run_name=run_name)  # Check if valid run
+        return self.runs[run_name]
+
+    @abstractmethod
+    def generate_predictions(
+        self,
+        run_name: str,
+        model: Type,
+        model_params: Optional[Union[Params, Dict[str, float]]],
+        tune_params=False,
+    ) -> ArrayLike:
+        pass
+
+    def evaluate(
+        self,
+        run_name: str,
+        metric_to_func: Dict[str, Callable],
+        labels: ArrayLike,
+    ) -> pd.DataFrame:
+        # Check preconditions
+        self._check_if_valid_run_name(run_name=run_name)  # Check if valid run
+        evaluator = self.runs[run_name]
+        if np.ndim(evaluator.preds) < 1:  # Check if predictions are a valid type
+            msg = f"Invalid type: {type(labels)} for predictions. Must be an iterable object."
+            raise _log_error(msg)
+        if np.ndim(labels) < 1:  # Check if labels are a valid type
+            msg = (
+                f"Invalid type: {type(labels)} for labels. Must be an iterable object."
+            )
+            raise _log_error(msg)
+        evaluator.labels = labels
+        # pyre-fixme[6]: Incompatible parameter type...
+        if len(evaluator.preds) != len(labels):  # Check lengths of preds and labels
+            msg = "Predictions and labels have unequal lengths."
+            raise _log_error(msg)
+
+        # Run evaluation
+        metric_to_result: Dict[str, ArrayLike] = {}
+        for metric, func in metric_to_func.items():
+            try:
+                metric_vals = func(evaluator.preds, evaluator.labels)
+            except ValueError as e:
+                msg = (
+                    f"Error running evaluation for metric {metric}. Full message:\n {e}"
+                )
+                raise _log_error(msg)
+            metric_to_result[metric] = [metric_vals]
+
+        # Save and return evaluation results
+        aggregated_results = pd.DataFrame(metric_to_result)
+        evaluator.results = aggregated_results
+        return evaluator.results
---
+++ kats/tests/test_evaluator.py
@@ -0,0 +1,102 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This file defines tests for the abstract Evaluator class
+
+import unittest
+import unittest.mock as mock
+
+import numpy as np
+import pandas as pd
+from kats.evaluation.evaluator import Evaluator, EvaluationObject
+from kats.tests.test_backtester_dummy_data import (
+    PROPHET_0_108_FCST_DUMMY_DATA,
+)
+from kats.utils.testing import error_funcs
+from pandas.testing import assert_frame_equal
+
+np.random.seed(42)
+
+# Constant Values
+FCST_EVALUATION_ERRORS = pd.DataFrame(
+    {  # Rounded to 6 decimals
+        "mape": [0.007622],
+        "smape": [0.007588],
+        "mae": [3.361111],
+        "mse": [12.916667],
+        "rmse": [3.593976],
+    }
+)
+
+
+class EvaluatorTest(unittest.TestCase):
+    @classmethod
+    @mock.patch.multiple(Evaluator, __abstractmethods__=set())
+    def setUpClass(cls):
+        cls.evaluator = Evaluator()
+
+    def test_create_evaluation_run(self) -> None:
+        self.evaluator.create_evaluation_run(run_name="valid_run")
+        self.assertEqual(
+            self.evaluator.runs["valid_run"],
+            EvaluationObject(None, None, None, None, None),
+        )
+
+    def test_create_invalid_run(self) -> None:
+        # Non string name
+        with self.assertRaises(ValueError):
+            # pyre-ignore[6]: Expected `str`
+            self.evaluator.create_evaluation_run(run_name=2)
+
+        # Duplicate
+        self.evaluator.create_evaluation_run(run_name="duplicate_run")
+        with self.assertRaises(ValueError):
+            self.evaluator.create_evaluation_run(run_name="duplicate_run"),
+
+    def test_delete_evaluation_run(self) -> None:
+        self.evaluator.create_evaluation_run(run_name="delete_run")
+        self.evaluator.delete_evaluation_run(run_name="delete_run")
+        with self.assertRaises(KeyError):
+            self.evaluator.runs["delete_run"]
+
+    def test_delete_invalid_run(self) -> None:
+        with self.assertRaises(ValueError):
+            self.evaluator.delete_evaluation_run(run_name="delete_invalid_run")
+
+    def test_get_evaluation_run(self):
+        self.evaluator.create_evaluation_run(run_name="retrieve_run")
+        self.assertEqual(
+            self.evaluator.get_evaluation_run(run_name="retrieve_run"),
+            EvaluationObject(None, None, None, None, None),
+        )
+
+    def test_get_invalid_evaluation_run(self) -> None:
+        with self.assertRaises(ValueError):
+            self.evaluator.get_evaluation_run(run_name="invalid_retrieve_run")
+
+    def test_evaluate(self):
+        # Set up data
+        PROPHET_0_108_FCST_DUMMY_DATA["rand_fcst"] = (
+            np.random.randint(1, 6, PROPHET_0_108_FCST_DUMMY_DATA.shape[0])
+            + PROPHET_0_108_FCST_DUMMY_DATA["fcst"]
+        )
+        labels = np.asarray(PROPHET_0_108_FCST_DUMMY_DATA["fcst"])
+        preds = np.asarray(PROPHET_0_108_FCST_DUMMY_DATA["rand_fcst"])
+
+        # Set up error funcs
+        errs = {}
+        for error_name, error_func in error_funcs.items():
+            if not error_name == "mase":
+                errs[error_name] = error_func
+
+        # Set up evaluator
+        self.evaluator.create_evaluation_run(run_name="test_evaluate")
+        self.evaluator.runs["test_evaluate"].preds = preds
+
+        eval_res = self.evaluator.evaluate(
+            run_name="test_evaluate", metric_to_func=errs, labels=labels
+        )
+        assert_frame_equal(
+            eval_res, FCST_EVALUATION_ERRORS, check_exact=False, check_less_precise=4
+        )
