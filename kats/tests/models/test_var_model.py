b34d55c87761325c744ef1f9208972d8c780576d
Jake Taylor <jakeetaylor@fb.com>
2022-02-01 15:51:03 -0800

    Add mocking, parametrization
    Summary:
    Improving unit tests for `test_var_model.py`:
    - Added pyre type checking
    - Mocking as necessary
    - Parameterization
    
    Next diff in stack will add explicit testing of model correctness.
    
    Reviewed By: michaelbrundage
    
    Differential Revision: D33907742
    
    fbshipit-source-id: 8c8513bdaea5077555a615f45c431290181a808a

--- kats/tests/models/test_var_model.py
+++ kats/tests/models/test_var_model.py
@@ -2,33 +2,37 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-unsafe
-
 import unittest
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from kats.consts import TimeSeriesData
 from kats.data.utils import load_data
 from kats.models.var import VARModel, VARParams
+from parameterized.parameterized import parameterized
+
+TEST_DATA = {
+    "multivariate": {
+        "ts": TimeSeriesData(load_data("multivariate_anomaly_simulated_data.csv"))
+    },
+    "multivariate_2": {"ts": TimeSeriesData(load_data("multi_ts.csv"))},
+}
 
 
 class testVARModel(TestCase):
-    def setUp(self):
-        DATA_multi = load_data("multivariate_anomaly_simulated_data.csv")
-        self.TSData_multi = TimeSeriesData(DATA_multi)
-        DATA_multi2 = load_data("multi_ts.csv")
-        self.TSData_multi2 = TimeSeriesData(DATA_multi2)
-
-    def test_fit_forecast(self) -> None:
+    # pyre-fixme[56]
+    @parameterized.expand(
+        [[TEST_DATA["multivariate"]["ts"]], [TEST_DATA["multivariate_2"]["ts"]]]
+    )
+    def test_fit_forecast(self, ts: TimeSeriesData) -> None:
         params = VARParams()
-        m = VARModel(self.TSData_multi, params)
+        m = VARModel(ts, params)
         m.fit()
         m.predict(steps=30, include_history=True)
 
-    def test_model_wrong_param(self) -> None:
+    def test_invalid_params(self) -> None:
         params = VARParams()
         input_data = TimeSeriesData(pd.DataFrame())
         with self.assertRaises(ValueError):
@@ -36,63 +40,82 @@ class testVARModel(TestCase):
             m.fit()
             m.predict(steps=30, include_history=True)
 
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate"]["ts"]]])
     @patch("pandas.concat")
-    def test_predict_exception(self, mock_obj) -> None:
+    def test_predict_exception(self, ts: TimeSeriesData, mock_obj: MagicMock) -> None:
         mock_obj.side_effect = Exception
         with self.assertRaisesRegex(
             Exception, "^Failed to generate in-sample forecasts for historical data"
         ):
             params = VARParams()
-            m = VARModel(self.TSData_multi, params)
+            m = VARModel(ts, params)
             m.fit()
             m.predict(steps=30, include_history=True)
 
-    def test_predict_unfit(self) -> None:
+    # pyre-fixme[56]
+    @parameterized.expand(
+        [[TEST_DATA["multivariate"]["ts"]], [TEST_DATA["multivariate_2"]["ts"]]]
+    )
+    def test_predict_unfit(self, ts: TimeSeriesData) -> None:
         with self.assertRaises(ValueError):
-            m = VARModel(self.TSData_multi, VARParams())
+            m = VARModel(ts, VARParams())
             m.predict(steps=30)
 
-    def test_trivial_path(self) -> None:
+    # pyre-fixme[56]
+    @parameterized.expand(
+        [[TEST_DATA["multivariate"]["ts"]], [TEST_DATA["multivariate_2"]["ts"]]]
+    )
+    def test_search_space(self, ts: TimeSeriesData) -> None:
         params = VARParams()
         params.validate_params()
-        m = VARModel(self.TSData_multi, params)
         with self.assertRaises(NotImplementedError):
             VARModel.get_parameter_search_space()
 
     # @pytest.mark.image_compare
-    def test_plot(self) -> plt.Figure:
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate_2"]["ts"]]])
+    def test_plot(self, ts: TimeSeriesData) -> plt.Figure:
         # Test the example from the 201 notebook.
-        m = VARModel(self.TSData_multi2, VARParams())
+        m = VARModel(ts, VARParams())
         m.fit()
         m.predict(steps=90)
         m.plot()
         return plt.gcf()
 
     # @pytest.mark.image_compare
-    def test_plot_include_history(self) -> plt.Figure:
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate_2"]["ts"]]])
+    def test_plot_include_history(self, ts: TimeSeriesData) -> plt.Figure:
         # This shouldn't error, but currently does.
         with self.assertRaises(ValueError):
-            m = VARModel(self.TSData_multi2, VARParams())
+            m = VARModel(ts, VARParams())
             m.fit()
             m.predict(steps=90, include_history=True)
             m.plot()
             return plt.gcf()
 
-    def test_plot_ax_not_supported(self) -> None:
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate"]["ts"]]])
+    def test_plot_ax_not_supported(self, ts: TimeSeriesData) -> None:
         with self.assertRaises(ValueError):
             _, ax = plt.subplots()
-            m = VARModel(self.TSData_multi, VARParams())
+            m = VARModel(ts, VARParams())
             m.fit()
             m.predict(steps=5)
             m.plot(ax=ax)
 
-    def test_plot_unpredict(self) -> None:
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate"]["ts"]]])
+    def test_plot_unpredict(self, ts: TimeSeriesData) -> None:
         with self.assertRaises(ValueError):
-            m = VARModel(self.TSData_multi, VARParams())
+            m = VARModel(ts, VARParams())
             m.plot()
 
-    def test_str(self) -> None:
-        result = str(VARModel(self.TSData_multi, VARParams()))
+    # pyre-fixme[56]
+    @parameterized.expand([[TEST_DATA["multivariate"]["ts"]]])
+    def test_str(self, ts: TimeSeriesData) -> None:
+        result = str(VARModel(ts, VARParams()))
         self.assertEqual("VAR", result)
 
 
