1090d0a52079b7efcddf1049cf2ee9ee5cb5857c
Xiaodong Jiang <iamxiaodong@fb.com>
2022-02-02 13:38:10 -0800

    Copy rights header updates
    Summary:
    We added linter rules for Kats python code and license, applied those rules to the entire Kats codebase.
    Three changes have been made:
    1. Updated lint engine rules in [fbsource-lint-engine.toml](https://www.internalfb.com/code/fbsource/tools/arcanist/lint/fbsource-lint-engine.toml), added two Python rules and license rules for `fbcode/kats/**`
    2. Updated lint license rules (i.e., MIT) in [fbsource-licenselint-config.toml](https://fburl.com/code/0r47qq5y) to check the MIT license headers in Kats
    3. Applied those changes to Kats code base, run `arc lint --apply-patches --take LICENSELINT --paths-cmd 'hg files fbcode/kats'` to apply patches.
    
    See [this wiki](https://www.internalfb.com/intern/wiki/Linting/License_Lint/#enabling-the-linter) for more details on license and lint rules.
    
    Reviewed By: zertosh
    
    Differential Revision: D33929583
    
    fbshipit-source-id: 242683823029569669166b17c3da4011ef847be9

--- kats/tests/detectors/test_changepoint_evaluator.py
+++ kats/tests/detectors/test_changepoint_evaluator.py
@@ -1,6 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 from datetime import datetime, timedelta
 from unittest import TestCase, mock
 
--- sphinx/make.bat
+++ sphinx/make.bat
@@ -1,3 +1,4 @@
+@nolint
 @ECHO OFF
 
 REM Command file for Sphinx documentation
