0b51a282197d40869b341229443fc28c191d133f
Steven Troxler <stroxler@fb.com>
2022-02-02 05:19:35 -0800

    remaining projects tstarting with k - o
    Summary:
    Convert type comments in osx
    
    Produced by running:
    ```
    python -m  libcst.tool codemod convert_type_comments.ConvertTypeComment osx
    ```
    from fbsource.
    
    See
    https://fb.workplace.com/groups/pythonfoundation/permalink/3106231549690303/
    
    Reviewed By: grievejia
    
    Differential Revision: D33904813
    
    fbshipit-source-id: 20cc19e541c2c5064f368d90fe3d6f2220b306df

--- kats/detectors/dtwcpd.py
+++ kats/detectors/dtwcpd.py
@@ -117,7 +117,7 @@ class DTWCPDDetector(Detector):
         self.MIN_TS_VALUE = min_value
         self.MAD_CUTOFF = mad_threshold
         self.MIN_VALUE = min_value
-        self.outliers = []  # type: List[DTWCPDChangePoint]
+        self.outliers: "List[DTWCPDChangePoint]" = []
         self.skip_size = skip_size
         self.match_against_same_time_series = match_against_same_time_series
 
