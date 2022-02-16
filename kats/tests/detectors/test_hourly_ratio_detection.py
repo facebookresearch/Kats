789b5751080540bb5e2706e8b2ae7ac4f17a3783
James Kunert-Graf <kunertgraf@fb.com>
2022-02-16 09:22:45 -0800

    Cleaned up test_hourly_ratio_detection using parameterized
    Summary:
    Cleaned up the test cases for the HourlyRatioDetector. As with D30346618 (https://github.com/facebookresearch/Kats/commit/931a45f35b0bf0258eb635bac0749e3b16210b90), this uses parameterized to reduce redundancy in the code, and made sure there was only one assertion for each test function.
    
    There are nine tests in the code, executed in the original code as follows:
    
    In `test_detector`:
    - Three valid datasets, basic functionality test attempts to run the HourlyRatioDetector and plot the result (for three generated datasets generated by `data_generation` under different parameters)
    
    In `test_other`:
    - Five tests of invalid datasets, test checks that ValueError is thrown as expected (two of these datasets are loaded from csv files, one is an empty dataset created in `setUp`, and the other two tests use a dataset generated with `data_generation`)
    - One test which uses the generated dataset to create a `HourlyRatioDetector` but attempts to generate a plot before running the `.detector()` method
    
    Using parameterized I consolidated the tests while moving `TimeSeriesData` generation to `setUpClass`. Note that I used this instead of `setUp` so as to initialize the timeseries once at the start of the tests, rather than before every test individually.
    
    Reviewed By: jeffhandl
    
    Differential Revision: D34158139
    
    fbshipit-source-id: 6201ea7be5f4ab313b147099b3de242d4cbaa549

--- kats/tests/detectors/test_hourly_ratio_detection.py
+++ kats/tests/detectors/test_hourly_ratio_detection.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import cast
+from typing import cast, Union
 from unittest import TestCase
 
 import numpy as np
@@ -11,67 +11,80 @@ import pandas as pd
 from kats.consts import TimeSeriesData
 from kats.data.utils import load_data
 from kats.detectors.hourly_ratio_detection import HourlyRatioDetector
+from parameterized import parameterized
 
 
 class HourlyRatioDectorTest(TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         daily_data = load_data("peyton_manning.csv")
         daily_data.columns = ["time", "y"]
-        self.ts_data_daily = TimeSeriesData(daily_data)
+        cls.invalid_daily_ts = TimeSeriesData(daily_data)
+        cls.invalid_minutely_ts = cls.data_generation(freq="T")
 
-        self.TSData_empty = TimeSeriesData(pd.DataFrame([], columns=["time", "y"]))
+        cls.invalid_empty_ts = TimeSeriesData(pd.DataFrame([], columns=["time", "y"]))
 
-        DATA_multi = load_data("multivariate_anomaly_simulated_data.csv")
-        self.TSData_multi = TimeSeriesData(DATA_multi)
+        multi_data = load_data("multivariate_anomaly_simulated_data.csv")
+        cls.invalid_multi_ts = TimeSeriesData(multi_data)
 
+        cls.valid_hourly_ts = cls.data_generation(drop=False)
+        cls.valid_missing_ts = cls.data_generation()
+        cls.valid_minutely_ts = cls.data_generation(freq="T")
+
+        plot_before_detector_ts = cls.data_generation(freq="T")
+        cls.plot_before_detector_hr = HourlyRatioDetector(
+            plot_before_detector_ts, freq="T", aggregate="max"
+        )
+
+    @classmethod
     def data_generation(
-        self, freq: str = "H", drop: bool = True, frac: float = 0.95
+        cls, freq: str = "H", drop: bool = True, frac: float = 0.95
     ) -> TimeSeriesData:
         time = pd.date_range("2018-01-01", "2020-01-01", freq=freq)
         n = len(time)
         x = np.arange(n)
-        values = np.abs(np.sin(30 * x) + 5 * x + 10 * x ** 2) + np.random.randn(n)
+        values = np.abs(np.sin(30 * x)) + 0.3 * np.random.randn(n)
         df = pd.DataFrame(values, columns=["value"])
         df["time"] = time
         if drop:
             df = cast(pd.DataFrame, df.sample(frac=frac, replace=False))
         return TimeSeriesData(df)
 
-    def test_detector(self) -> None:
+    # pyre-ignore[16] Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(
+        [  # name, freq, aggregate
+            ["valid_hourly_ts", "H", None],
+            ["valid_missing_ts", "H", None],
+            ["valid_minutely_ts", "T", "max"],
+        ]
+    )
+    def test_detector(self, name: str, freq: str, aggregate: Union[str, None]) -> None:
         # test hourly data without missing vlaues
-        ts = self.data_generation(drop=False)
-        hr = HourlyRatioDetector(ts)
+        ts = getattr(self, name)
+        hr = HourlyRatioDetector(ts, freq=freq, aggregate=aggregate)
         hr._preprocess()
         hr.detector()
         hr.plot()
 
-        # test hourly data with missing values
-        ts = self.data_generation()
-        hr = HourlyRatioDetector(ts, freq="H")
-        hr._preprocess()
-        hr.detector()
-        hr.plot()
+    # pyre-ignore[16] Module parameterized.parameterized has no attribute expand.
+    @parameterized.expand(
+        [
+            ["invalid_daily_ts"],
+            ["invalid_minutely_ts"],
+            ["invalid_multi_ts"],
+            ["invalid_empty_ts"],
+        ]
+    )
+    def test_invalid(self, name: str) -> None:
+        ts = getattr(self, name)
+        self.assertRaises(ValueError, HourlyRatioDetector, ts)
 
-        # test minutely data with missing values
-        ts = self.data_generation(freq="T")
-        hr = HourlyRatioDetector(ts, freq="T", aggregate="max")
-        hr._preprocess()
-        hr.detector()
-        hr.plot()
-
-    def test_other(self) -> None:
-        self.assertRaises(ValueError, HourlyRatioDetector, self.TSData_multi)
-
-        self.assertRaises(ValueError, HourlyRatioDetector, self.ts_data_daily)
-
-        self.assertRaises(ValueError, HourlyRatioDetector, self.TSData_empty)
-
-        ts = self.data_generation(freq="T")
-        self.assertRaises(ValueError, HourlyRatioDetector, data=ts)
-
+    def test_minutely_other_aggregate(self) -> None:
+        ts = self.invalid_minutely_ts
         self.assertRaises(
             ValueError, HourlyRatioDetector, data=ts, aggregate="other_method"
         )
 
-        hr = HourlyRatioDetector(ts, freq="T", aggregate="max")
+    def test_plot_before_detector(self) -> None:
+        hr = self.plot_before_detector_hr
         self.assertRaises(ValueError, hr.plot)
