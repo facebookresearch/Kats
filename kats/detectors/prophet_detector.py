Fixing ProphetDetectorModel Z-score computation (part 1/2)
Summary:
ProphetDetectorModel's Z-score computation contains a number of bugs/issues:
1. The old implementation assumes that Prophet's confidence interval (CI) is computed in terms of the sample stddev, where `uncertainty_samples` is the number of samples. In fact, this is not the case -- Prophet's CI is computed from population stats. This results in the Z-score being incorrectly scaled by sqrt(`uncertainty_samples`).
2. Prophet's CI is symmetric, so there is no need to separately compute upper/lower stddev.
3. There was a bug where the Z-score was computed by dividing by the CI percentage (e.g., 80%) rather than the two-tailed Z-statistic (e.g., `scipy.stats.norm.ppf(0.8/2 + 0.5)` = 1.282).

This diff fixes the first two issues, but retains the scale as a constant factor on the Z-score to ensure backwards compatibility when using default values for `uncertainty_samples`. A subsequent diff will fix the third issue.

After this change, `uncertainty_samples` can be changed without skewing ProphetDetectorModel's Z-scores.

Reviewed By: yangbk560

Differential Revision: D41512695

fbshipit-source-id: f4c09e7c655ea5eca154646cfd6160e4242bbe81

kats/detectors/prophet_detector.py
@@ -27,7 +27,11 @@ PROPHET_YHAT_COLUMN = "yhat"
 PROPHET_YHAT_LOWER_COLUMN = "yhat_lower"
 PROPHET_YHAT_UPPER_COLUMN = "yhat_upper"
 
-
+# Previously assumed Prophet CI width was computed based on sample stddev,
+# where uncertainty_samples was num of samples. This scale constant ensures that the
+# corrected Z-score is scaled by the same amount as the original score when
+# using default values.
+Z_SCORE_SCALE_CONST: float = (50**0.5) / 2
 MIN_STDEV = 1e-9
 PREDICTION_UNCERTAINTY_SAMPLES = 50
 OUTLIER_REMOVAL_UNCERTAINTY_SAMPLES = 40
@@ -59,7 +63,6 @@ def deviation_from_predicted_val(
     data: TimeSeriesData,
     predict_df: pd.DataFrame,
     ci_threshold: Optional[float] = None,
-    uncertainty_samples: Optional[float] = None,
 ) -> Union[pd.Series, pd.DataFrame]:
     return (data.value - predict_df[PROPHET_YHAT_COLUMN]) / predict_df[
         PROPHET_YHAT_COLUMN
@@ -70,37 +73,22 @@ def z_score(
     data: TimeSeriesData,
     predict_df: pd.DataFrame,
     ci_threshold: float = 0.8,
-    uncertainty_samples: float = PREDICTION_UNCERTAINTY_SAMPLES,
 ) -> Union[pd.Series, pd.DataFrame]:
-    # asymmetric confidence band => points above the prediction use upper bound in calculation, points below the prediction use lower bound
-
-    actual_upper_std = (
-        (uncertainty_samples**0.5)
-        * (predict_df[PROPHET_YHAT_UPPER_COLUMN] - predict_df[PROPHET_YHAT_COLUMN])
-        / ci_threshold
-    )
-    actual_lower_std = (
-        (uncertainty_samples**0.5)
-        * (predict_df[PROPHET_YHAT_COLUMN] - predict_df[PROPHET_YHAT_LOWER_COLUMN])
+    actual_scaled_std = (
+        Z_SCORE_SCALE_CONST
+        * (
+            predict_df[PROPHET_YHAT_UPPER_COLUMN]
+            - predict_df[PROPHET_YHAT_LOWER_COLUMN]
+        )
         / ci_threshold
     )
 
     # if std is 0, set it to a very small value to prevent division by zero in next step
-    upper_std = np.maximum(actual_upper_std, MIN_STDEV)
-    lower_std = np.maximum(actual_lower_std, MIN_STDEV)
+    scaled_std = np.maximum(actual_scaled_std, MIN_STDEV)
 
-    upper_score = (
-        (data.value > predict_df[PROPHET_YHAT_COLUMN])
-        * (data.value - predict_df[PROPHET_YHAT_COLUMN])
-        / upper_std
-    )
-    lower_score = (
-        (data.value < predict_df[PROPHET_YHAT_COLUMN])
-        * (data.value - predict_df[PROPHET_YHAT_COLUMN])
-        / lower_std
-    )
+    score = (data.value - predict_df[PROPHET_YHAT_COLUMN]) / scaled_std
 
-    return upper_score + lower_score
+    return score
 
 
 class ProphetScoreFunction(Enum):
@@ -313,7 +301,6 @@ class ProphetDetectorModel(DetectorModel):
                     data=data,
                     predict_df=predict_df,
                     ci_threshold=model.interval_width,
-                    uncertainty_samples=model.uncertainty_samples,
                 ),
             ),
             confidence_band=confidence_band,