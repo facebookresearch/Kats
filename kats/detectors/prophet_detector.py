Fixing ProphetDetectorModel Z-score computation (part 2/2)
Summary:
ProphetDetectorModel's Z-score computation contained a number of bugs/issues:
1. The old implementation assumed that Prophet's confidence interval (CI) is computed in terms of the sample stddev, where `uncertainty_samples` is the number of samples. In fact, this is not the case -- Prophet's CI is computed from population stats. This resulted in the Z-score being incorrectly scaled by sqrt(`uncertainty_samples`).
2. Prophet's CI is symmetric, so there is no need to separately compute upper/lower stddev.
3. There was a bug where the Z-score was computed by dividing by the CI percentage (e.g., 80%) rather than the two-tailed Z-statistic (e.g., `scipy.stats.norm.ppf(0.8/2 + 0.5)` = 1.282).

This diff fixes the third issue, but retains the scale as a constant factor on the Z-score to ensure backwards compatibility when using default values for `scoring_confidence_interval`.

After this change, `scoring_confidence_interval` can be changed without skewing ProphetDetectorModel's Z-scores.

Reviewed By: yangbk560

Differential Revision: D41512759

fbshipit-source-id: 37b6a272bdd5fd0bdcc91294a482b7a96b52ed30

kats/detectors/prophet_detector.py
@@ -20,6 +20,7 @@ from kats.consts import DEFAULT_VALUE_NAME, TimeSeriesData
 from kats.detectors.detector import DetectorModel
 from kats.detectors.detector_consts import AnomalyResponse, ConfidenceBand
 from kats.models.prophet import predict
+from scipy.stats import norm
 
 PROPHET_TIME_COLUMN = "ds"
 PROPHET_VALUE_COLUMN = "y"
@@ -28,10 +29,12 @@ PROPHET_YHAT_LOWER_COLUMN = "yhat_lower"
 PROPHET_YHAT_UPPER_COLUMN = "yhat_upper"
 
 # Previously assumed Prophet CI width was computed based on sample stddev,
-# where uncertainty_samples was num of samples. This scale constant ensures that the
+# where uncertainty_samples was num of samples. Also previously mistakenly
+# used CI width in place of Z-statistic. These scale constants ensure that the
 # corrected Z-score is scaled by the same amount as the original score when
-# using default values.
-Z_SCORE_SCALE_CONST: float = (50**0.5) / 2
+# using default values, but otherwise acts as a true Z-score.
+Z_SCORE_CI_THRESHOLD_SCALE_CONST: float = norm.ppf(0.8 / 2 + 0.5) / 0.8
+Z_SCORE_SCALE_CONST: float = (50**0.5) * Z_SCORE_CI_THRESHOLD_SCALE_CONST / 2
 MIN_STDEV = 1e-9
 PREDICTION_UNCERTAINTY_SAMPLES = 50
 OUTLIER_REMOVAL_UNCERTAINTY_SAMPLES = 40
@@ -63,6 +66,7 @@ def deviation_from_predicted_val(
     data: TimeSeriesData,
     predict_df: pd.DataFrame,
     ci_threshold: Optional[float] = None,
+    **kwargs: Any,
 ) -> Union[pd.Series, pd.DataFrame]:
     return (data.value - predict_df[PROPHET_YHAT_COLUMN]) / predict_df[
         PROPHET_YHAT_COLUMN
@@ -73,15 +77,22 @@ def z_score(
     data: TimeSeriesData,
     predict_df: pd.DataFrame,
     ci_threshold: float = 0.8,
+    use_legacy_z_score: bool = True,
+    **kwargs: Any,
 ) -> Union[pd.Series, pd.DataFrame]:
-    actual_scaled_std = (
-        Z_SCORE_SCALE_CONST
-        * (
-            predict_df[PROPHET_YHAT_UPPER_COLUMN]
-            - predict_df[PROPHET_YHAT_LOWER_COLUMN]
-        )
-        / ci_threshold
+    ci_width = (
+        predict_df[PROPHET_YHAT_UPPER_COLUMN] - predict_df[PROPHET_YHAT_LOWER_COLUMN]
     )
+    if use_legacy_z_score:
+        actual_scaled_std = (
+            (Z_SCORE_SCALE_CONST / Z_SCORE_CI_THRESHOLD_SCALE_CONST)
+            * ci_width
+            / ci_threshold
+        )
+    else:
+        actual_scaled_std = (
+            Z_SCORE_SCALE_CONST * ci_width / norm.ppf(ci_threshold / 2 + 0.5)
+        )
 
     # if std is 0, set it to a very small value to prevent division by zero in next step
     scaled_std = np.maximum(actual_scaled_std, MIN_STDEV)
@@ -140,6 +151,7 @@ class ProphetDetectorModel(DetectorModel):
         uncertainty_samples: float = PREDICTION_UNCERTAINTY_SAMPLES,
         outlier_removal_uncertainty_samples: int = OUTLIER_REMOVAL_UNCERTAINTY_SAMPLES,
         vectorize: bool = False,
+        use_legacy_z_score: bool = True,
     ) -> None:
         if serialized_model:
             self.model = model_from_json(serialized_model)
@@ -167,6 +179,7 @@ class ProphetDetectorModel(DetectorModel):
         else:
             self.uncertainty_samples: float = 0
         self.vectorize = vectorize
+        self.use_legacy_z_score = use_legacy_z_score
 
     def serialize(self) -> bytes:
         """Serialize the model into a json.
@@ -301,6 +314,7 @@ class ProphetDetectorModel(DetectorModel):
                     data=data,
                     predict_df=predict_df,
                     ci_threshold=model.interval_width,
+                    use_legacy_z_score=self.use_legacy_z_score,
                 ),
             ),
             confidence_band=confidence_band,

kats/tests/detectors/test_prophet_detector.py
@@ -236,15 +236,6 @@ class TestProphetDetector(TestCase):
 
         return merged_ts
 
-    def calc_std(
-        self, predicted_val: float, upper_bound: float, lower_bound: float
-    ) -> float:
-        actual_std = (50**0.5) * ((upper_bound - lower_bound) / 2) / 0.8
-
-        std = max(actual_std, 1e-9)
-
-        return std
-
     def calc_z_score(
         self,
         actual_val: float,
@@ -252,7 +243,9 @@ class TestProphetDetector(TestCase):
         upper_bound: float,
         lower_bound: float,
     ) -> float:
-        std = self.calc_std(predicted_val, upper_bound, lower_bound)
+        # Assumes default ProphetDetectorModel param values
+        actual_std = (50**0.5) * ((upper_bound - lower_bound) / 2) / 0.8
+        std = max(actual_std, 1e-9)
 
         return (actual_val - predicted_val) / std
 
@@ -483,6 +476,7 @@ class TestProphetDetector(TestCase):
         # add anomaly at index 95
         ts.value[95] += 100
 
+        np.random.seed(0)
         z_score_model = ProphetDetectorModel(score_func=ProphetScoreFunction.z_score)
         z_score_response = z_score_model.fit_predict(ts[90:], ts[:90])
         actual_z_score = self.calc_z_score(
@@ -510,6 +504,37 @@ class TestProphetDetector(TestCase):
         self.assertNotEqual(
             z_score_response.confidence_band.lower,
             z_score_response.predicted_ts,
+        )
+
+        # Corrected Z-score should be the same as legacy Z-score if using default
+        # scoring confidence interval
+        np.random.seed(0)
+        legacy_z_score_model = ProphetDetectorModel(
+            score_func=ProphetScoreFunction.z_score, use_legacy_z_score=False
+        )
+        legacy_z_score_response = legacy_z_score_model.fit_predict(ts[90:], ts[:90])
+        self.assertAlmostEqual(
+            legacy_z_score_response.scores.value[5],
+            # pyre-fixme[6]: For 2nd param expected `SupportsRSub[Variable[_T],
+            #  SupportsAbs[SupportsRound[object]]]` but got `float`.
+            actual_z_score,
+            places=5,
+        )
+
+        # If using custom scoring confidence interval, corrected Z-scores will differ
+        np.random.seed(0)
+        legacy_z_score_model = ProphetDetectorModel(
+            score_func=ProphetScoreFunction.z_score,
+            use_legacy_z_score=False,
+            scoring_confidence_interval=0.9,
+        )
+        legacy_z_score_response = legacy_z_score_model.fit_predict(ts[90:], ts[:90])
+        self.assertNotAlmostEqual(
+            legacy_z_score_response.scores.value[5],
+            # pyre-fixme[6]: For 2nd param expected `SupportsRSub[Variable[_T],
+            #  SupportsAbs[SupportsRound[object]]]` but got `float`.
+            actual_z_score,
+            places=5,
         )
 
     # pyre-fixme[56]: Pyre was not able to infer the type of the decorator