20154ed6ba0f5a71e6b8ffbe51e256af2d2a475f
Peiyi Zhang <peiyizhang@meta.com>
2023-03-23 23:10:29 -0700

    rolling iqr/z/mad
    Summary:
    rolling iqr/z/mad
    modified z, denominator =MAD/IQR, numerator=X- median
    
    Design doc: https://docs.google.com/document/d/1xeG9lB4oXmDlgSAMV2vtPhh38q3H4E2RjDeleZmw9sY/edit
    
    Reviewed By: yl3829, uthakore
    
    Differential Revision:
    D42608322
    
    Privacy Context Container: L1153866
    
    fbshipit-source-id: d04f0972c643efd588c17899b84d4d7107adcba5

---
+++ kats/detectors/rolling_stats_model.py
@@ -0,0 +1,398 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import json
+import logging
+from enum import Enum
+from typing import Any, Callable, Dict, Optional, Union
+
+import numpy as np
+import pandas as pd
+from kats.consts import DataError, InternalError, ParameterError, TimeSeriesData
+
+from kats.detectors.cusum_model import SeasonalityHandler
+from kats.detectors.detector import DetectorModel
+from kats.detectors.detector_consts import AnomalyResponse
+from scipy import stats
+
+
+SEASON_PERIOD_FREQ_MAP: Dict[str, int] = {
+    "daily": 1,
+    "weekly": 7,
+    "monthly": 30,
+    "yearly": 365,
+}
+
+
+class RollStatsFunction(Enum):
+    iqr = "iqr"
+    mad = "mad"
+    z_score = "z_score"
+    modified_z_score_mad = "modified_z_score_mad"
+    modified_z_score_iqr = "modified_z_score_iqr"
+
+
+DEFAULT_STATS_SCORE_FUNCTION: RollStatsFunction = RollStatsFunction.z_score
+
+STR_TO_STATS_SCORE_FUNC: Dict[str, RollStatsFunction] = {
+    # Used for param tuning
+    val.name: val
+    for val in RollStatsFunction
+}
+
+
+def calculate_iqr(data_list: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Calculate IQR = Q3 - Q1
+    """
+    data_list_2dim = data_list.reshape((-1, data_list.shape[-1]))
+    q3, q1 = np.nanpercentile(data_list_2dim, [75, 25], axis=1)
+    result = q3 - q1
+    return result[0] if len(result) == 1 else result
+
+
+def calculate_z_scores(data_list: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Calculate the z-score of the last data point in data_list.
+    Or calculate the z-score of the last data point in each row of the data_list.
+    if data in the window (one row) is all nan, then return nan for that row.
+    """
+    data_list_2dim = data_list.reshape((-1, data_list.shape[-1]))
+
+    m = np.nanmean(data_list_2dim[:, :-1], axis=1)
+    d = np.nanstd(data_list_2dim[:, :-1], axis=1)
+
+    numerator = data_list_2dim[:, -1] - m
+    result = np.divide(numerator, d, out=np.zeros_like(numerator), where=d != 0)
+
+    return result[0] if len(result) == 1 else result
+
+
+def calculate_mad(data_list: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Calculate MAD: the mean (average) distance between each data
+    value and the mean of the data set.
+    """
+    data_list_2dim = data_list.reshape((-1, data_list.shape[-1]))
+    result = stats.median_abs_deviation(data_list_2dim, axis=1, nan_policy="omit")
+
+    # pyre-ignore[6, 16]
+    return result[0] if len(result) == 1 else result
+
+
+def calculate_modified_z_scores_mad(data_list: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Calculate Modified z-score: (x-median)/MAD.
+    x: the last point of data_list.
+    median: median of data_list except the last point
+    MAD: MAD of data_list except the last point
+    """
+    data_list_2dim = data_list.reshape((-1, data_list.shape[-1]))
+    m = np.nanmedian(data_list_2dim[:, :-1], axis=1)
+    d = calculate_mad(data_list_2dim[:, :-1])
+
+    numerator = data_list_2dim[:, -1] - m
+    result = np.divide(numerator, d, out=np.zeros_like(numerator), where=d != 0)
+
+    return result[0] if len(result) == 1 else result
+
+
+def calculate_modified_z_scores_iqr(data_list: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Calculate Modified z-score (iqr version): (x-median)/IQR
+    x: the last point of data_list.
+    median: median of data_list except the last point
+    IQR: IQR of data_list except the last point
+    """
+    data_list_2dim = data_list.reshape((-1, data_list.shape[-1]))
+    m = np.nanmedian(data_list_2dim[:, :-1], axis=1)
+    d = calculate_iqr(data_list_2dim[:, :-1])
+
+    numerator = data_list_2dim[:, -1] - m
+    result = np.divide(numerator, d, out=np.zeros_like(numerator), where=d != 0)
+
+    return result[0] if len(result) == 1 else result
+
+
+# pyre-ignore[24]
+SCORE_FUNC_DICT: Dict[str, Callable] = {
+    RollStatsFunction.iqr.value: calculate_iqr,
+    RollStatsFunction.z_score.value: calculate_z_scores,
+    RollStatsFunction.mad.value: calculate_mad,
+    RollStatsFunction.modified_z_score_mad.value: calculate_modified_z_scores_mad,
+    RollStatsFunction.modified_z_score_iqr.value: calculate_modified_z_scores_iqr,
+}
+
+_log: logging.Logger = logging.getLogger("rolling_Stats_model")
+
+
+class RollingStatsModel(DetectorModel):
+    """RollingStatsModel
+    It includes calculating z-scores, IQR-scores, MAD-scores, IQR/MAD based modified z-scores
+
+    Attributes:
+        rolling_window: int. Either in terms of seconds or number of points.
+            rolling window size for calculating MAD, IQR, std, average.
+        serialized_model: Optional[bytes] = None.
+        statistics: Union[str, RollStatsFunction]. Default is z_score.
+        remove_seasonality: bool = False. Whether removing seasonality when running the detection model.
+        point_based: bool = True. Matched with parameter rolling_window's unit.
+        seasonality_period: str = "daily". Seasonality period for seasonality decomposition.
+        score_base: float = 1.0. For modified z scores. Multiplier for the denominator.
+
+    Example:
+    >>> model = RollingStatsModel(
+            rolling_window=10,
+            statistics="z_score",
+            remove_seasonality=True,
+            point_based=True,
+        )
+    >>> anom = model.fit_predict(historical_data=hist_ts, data=test_ts)
+    >>> anom.scores.plot()
+    """
+
+    def __init__(
+        self,
+        rolling_window: int,
+        serialized_model: Optional[bytes] = None,
+        statistics: Union[str, RollStatsFunction] = DEFAULT_STATS_SCORE_FUNCTION,
+        remove_seasonality: bool = False,
+        point_based: bool = True,
+        seasonality_period: str = "daily",
+        score_base: float = 1.0,
+    ) -> None:
+        if serialized_model:
+            previous_model = json.loads(serialized_model)
+            self.rolling_window: int = previous_model["rolling_window"]
+            self.statistics: RollStatsFunction = previous_model["statistics"]
+            self.remove_seasonality: bool = previous_model["remove_seasonality"]
+            self.point_based: bool = previous_model["point_based"]
+            self.seasonality_period: str = previous_model["seasonality_period"]
+
+        else:
+            self.rolling_window: int = rolling_window
+
+            # We allow stats_score_function to be a str for compatibility with param tuning
+            if isinstance(statistics, str):
+                if statistics in STR_TO_STATS_SCORE_FUNC:
+                    self.statistics: RollStatsFunction = STR_TO_STATS_SCORE_FUNC[
+                        statistics
+                    ]
+                else:
+                    self.statistics: RollStatsFunction = DEFAULT_STATS_SCORE_FUNCTION
+                    _log.info(
+                        "Invalid Statstics name. Using default score function: z_score."
+                    )
+            else:
+                self.statistics: RollStatsFunction = statistics
+
+            self.remove_seasonality: bool = remove_seasonality
+            self.point_based: bool = point_based
+            self.seasonality_period: str = seasonality_period
+
+            # For denominator == MAD, it should be 0.6745
+            self.score_base: float = score_base
+
+            # We're creating 2 types of scores:
+            # IQR/MAD: rolling stats that are computed on a window of length w inclusive of the current point
+            # Z-scores: scores on the current point compared against a mean/median and stddev
+            #     or equivalent computed on a window of length w exclusive of the current point.
+            # Thus, for Z_scores, rolling window will be extended by 1.
+            if self.statistics.value in {"mad", "iqr"}:
+                self.extend_rolling_window: bool = False
+            else:
+                self.extend_rolling_window: bool = True
+
+    def serialize(self) -> bytes:
+        """
+        Retrun serilized model.
+        """
+        return str.encode(json.dumps(self.__dict__))
+
+    def _point_based_vectorized_data(self, data: np.ndarray) -> np.ndarray:
+        """
+        For Z-scores and its variants,
+        reshape the input data to shape (-1, self.rolling_window + 1)
+        Each row consists of [history window | one datapoint to evaluate]
+
+        For IQR and MAD,
+        reshape the input data to shape (-1, self.rolling_window)
+        """
+        if self.extend_rolling_window:
+            shape = data.shape[:-1] + (
+                data.shape[-1] - self.rolling_window,
+                self.rolling_window + 1,
+            )
+        else:
+            shape = data.shape[:-1] + (
+                data.shape[-1] - self.rolling_window + 1,
+                self.rolling_window,
+            )
+        strides = data.strides + (data.strides[-1],)
+        return np.lib.stride_tricks.as_strided(
+            data, shape=shape, strides=strides, writeable=False
+        )
+
+    def _fit_predict_point_based(
+        self,
+        data_interp: TimeSeriesData,
+        n_points_hist_data: int,
+    ) -> AnomalyResponse:
+        if n_points_hist_data <= self.rolling_window:
+            data_value = data_interp.value.values
+            reorg_data = self._point_based_vectorized_data(
+                np.concatenate(
+                    [
+                        np.nan * np.zeros(self.rolling_window - n_points_hist_data),
+                        data_value,
+                    ],
+                    0,
+                )
+            )
+        else:
+            data_value = data_interp.value.values[
+                (n_points_hist_data - self.rolling_window) :
+            ]
+            reorg_data = self._point_based_vectorized_data(data_value)
+
+        scores_array = (
+            SCORE_FUNC_DICT[self.statistics.value](reorg_data) * self.score_base
+        )
+        scores_tsd = TimeSeriesData(
+            time=data_interp.time[n_points_hist_data:], value=pd.Series(scores_array)
+        )
+
+        return AnomalyResponse(
+            scores=scores_tsd,
+            confidence_band=None,
+            predicted_ts=None,
+            anomaly_magnitude_ts=TimeSeriesData(
+                time=scores_tsd.time, value=pd.Series(np.zeros(len(scores_tsd.time)))
+            ),
+            stat_sig_ts=None,
+        )
+
+    def _check_window_sizes(self, frequency_sec: int) -> None:
+        if self.rolling_window / frequency_sec < 3:
+            error_info = (
+                "Please use a larger rolling window size, which at least "
+                f"includes 3 data points in the time series data, i.e., rolling_window > {3*frequency_sec}."
+            )
+            _log.error(error_info)
+            raise ParameterError(error_info)
+
+    def fit_predict(
+        self,
+        data: TimeSeriesData,
+        historical_data: Optional[TimeSeriesData] = None,
+        **kwargs: Any,
+    ) -> AnomalyResponse:
+        if not data.is_univariate():
+            raise DataError(
+                "Multiple time series not supported for Rolling-Stats algorithm."
+            )
+
+        if historical_data and not historical_data.is_univariate():
+            raise DataError(
+                "Multiple time series not supported for Rolling-Stats algorithm."
+            )
+
+        # pull all the data in historical data
+        if historical_data is not None:
+            n_points_hist_data = len(historical_data)
+            historical_data.extend(data, validate=False)
+        else:
+            # When historical_data is not provided, will use part of data as
+            # historical_data, and fill with zero anomaly score.
+            historical_data = data[:]
+            n_points_hist_data = 0
+
+        if self.remove_seasonality:
+            sh_data = SeasonalityHandler(
+                data=historical_data, seasonal_period=self.seasonality_period
+            )
+            historical_data = sh_data.remove_seasonality()
+
+        if self.point_based:
+            return self._fit_predict_point_based(
+                data_interp=historical_data,
+                n_points_hist_data=n_points_hist_data,
+            )
+
+        # pd.timedelta
+        frequency = historical_data.infer_freq_robust()
+        frequency_sec = int(frequency.total_seconds())
+        self._check_window_sizes(frequency_sec)
+
+        if (
+            self.rolling_window % frequency_sec == 0
+            and not historical_data.is_data_missing()
+        ):
+            self.rolling_window = self.rolling_window // frequency_sec
+            return self._fit_predict_point_based(
+                data_interp=historical_data,
+                n_points_hist_data=n_points_hist_data,
+            )
+
+        else:
+            total_data_df = historical_data.to_dataframe()
+            total_data_df.columns = ["time", "value"]
+            total_data_df = total_data_df.set_index("time")
+
+            scores = []
+            for i in range(len(data.time)):
+                # TODO: this loop could be further optimized by vectorization
+                test_end_dt = pd.to_datetime(data.time[i])
+                test_start_dt = test_end_dt - pd.Timedelta(
+                    str(self.rolling_window) + "s"
+                )
+                window_data_array = total_data_df[
+                    test_start_dt:test_end_dt
+                ].value.values
+                if len(window_data_array) >= 2:
+                    score = (
+                        SCORE_FUNC_DICT[self.statistics.value](window_data_array)
+                        * self.score_base
+                    )
+                else:
+                    score = np.nan
+                scores.append(score)
+
+            scores_tsd = TimeSeriesData(
+                pd.DataFrame({"time": data.time, "value": scores})
+            )
+
+            return AnomalyResponse(
+                scores=scores_tsd,
+                confidence_band=None,
+                predicted_ts=None,
+                anomaly_magnitude_ts=TimeSeriesData(
+                    time=data.time, value=pd.Series([0] * len(data.time))
+                ),
+                stat_sig_ts=None,
+            )
+
+    def fit(
+        self,
+        data: TimeSeriesData,
+        historical_data: Optional[TimeSeriesData] = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        fit is not implemented
+        """
+        raise InternalError("fit is not implemented, call fit_predict() instead")
+
+    def predict(
+        self,
+        data: TimeSeriesData,
+        historical_data: Optional[TimeSeriesData] = None,
+        **kwargs: Any,
+    ) -> AnomalyResponse:
+        """
+        predict is not implemented
+        """
+        raise InternalError("predict is not implemented, call fit_predict() instead")
---
+++ kats/tests/detectors/test_rolling_stats_model.py
@@ -0,0 +1,368 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from datetime import datetime
+from unittest import TestCase
+
+import numpy as np
+import pandas as pd
+from kats.consts import DataError, InternalError, ParameterError, TimeSeriesData
+from kats.detectors.rolling_stats_model import RollingStatsModel
+from parameterized.parameterized import parameterized
+
+
+def generate_ts_data(
+    granularities_sec: int = 60,
+    length: int = 100,
+    date_start_str: str = "2020-01-01 00:00:00",
+    include_start_point: bool = True,
+) -> TimeSeriesData:
+    np.random.seed(0)
+    date_start = datetime.strptime(date_start_str, "%Y-%m-%d %H:%M:%S")
+    ts_val = np.random.normal(10, 3, length)
+    if include_start_point:
+        ts_time = pd.date_range(
+            start=date_start, freq=str(granularities_sec) + "s", periods=length
+        )
+    else:
+        ts_time = pd.date_range(
+            start=date_start, freq=str(granularities_sec) + "s", periods=length + 1
+        )[1:]
+    ts = TimeSeriesData(pd.DataFrame({"time": ts_time, "value": pd.Series(ts_val)}))
+    return ts
+
+
+def generate_data_with_sudden_granularity_changes(length: int = 200) -> TimeSeriesData:
+    """
+    Generate data with sudden changes in data granularity
+    (e.g., before 2 weeks ago, hourly data; after 2 weeks ago, 15 minute granularity)
+    Common with ODS data because of ODS rollups
+    """
+    np.random.seed(0)
+    len_list = [length // 4, length // 4, length // 4, length - length // 4 * 3]
+    gran_list = [3600, 900, 600, 300]
+    ts = None
+    for i in range(3):
+        if ts is None:
+            ts = generate_ts_data(
+                granularities_sec=gran_list[i],
+                length=len_list[i],
+                date_start_str="2020-01-01 00:00:00",
+            )
+        else:
+            ts0 = generate_ts_data(
+                granularities_sec=gran_list[i],
+                length=len_list[i],
+                date_start_str=str(ts.time.iloc[-1]),
+                include_start_point=False,
+            )
+            ts.extend(ts0, validate=False)
+    assert ts is not None
+    return ts
+
+
+def generate_data_with_individual_missing_datapoints(
+    granularities_sec: int = 60,
+    length: int = 100,
+) -> TimeSeriesData:
+    """
+    Generate data with individual missing datapoints
+    Most often found when counting in Scuba without fills
+    """
+    np.random.seed(0)
+
+    ts = generate_ts_data(
+        granularities_sec=granularities_sec,
+        length=length,
+    )
+
+    missing_points = sorted(set({length // 3, length // 3 * 2, length - 3}))
+    ts_1, ts_2, ts_3, ts_4 = (
+        ts[: missing_points[0]],
+        ts[missing_points[0] + 1 : missing_points[1]],
+        ts[missing_points[1] + 1 : missing_points[2]],
+        ts[missing_points[2] + 1 :],
+    )
+
+    ts_1.extend(ts_2, validate=False)
+    ts_1.extend(ts_3, validate=False)
+    ts_1.extend(ts_4, validate=False)
+
+    return ts_1
+
+
+def generate_irregular_granularity_data(
+    granularities_sec: int = 60,
+    length: int = 200,
+    percentage: float = 0.7,
+    seed: int = 0,
+) -> TimeSeriesData:
+    """
+    Generate irregular granularity data
+    Most often found in ODS
+    """
+    np.random.seed(seed)
+    n = int(length * percentage)
+
+    ts_val = np.random.normal(10, 3, n)
+    ts_time = pd.date_range(
+        start="2020-01-01", freq=str(granularities_sec) + "s", periods=length
+    )[np.random.choice(list(range(length)), n)]
+
+    ts = TimeSeriesData(pd.DataFrame({"time": ts_time, "value": pd.Series(ts_val)}))
+    return ts
+
+
+class TestRollingStatsModels(TestCase):
+    # pyre-ignore[56]
+    @parameterized.expand(
+        [
+            # score func, window, point_based, seasonality
+            ("z_score", 10, False, False),
+            ("z_score", 10, True, False),
+            ("z_score", 20, True, False),
+            ("z_score", 20, False, False),
+            ("MAD", 10, True, False),
+            ("MAD", 10, False, False),
+            ("IQR", 20, True, False),
+            ("IQR", 20, False, False),
+            ("modified_z_score_mad", 10, True, False),
+            ("modified_z_score_mad", 10, False, False),
+            ("modified_z_score_iqr", 20, True, False),
+            ("modified_z_score_iqr", 20, False, False),
+        ]
+    )
+    def test_irregular_granularity_data(
+        self,
+        score_func: str,
+        window_size: int,
+        point_based: bool,
+        remove_seasonality: bool,
+    ) -> None:
+        ig_ts = generate_irregular_granularity_data(percentage=0.85)
+
+        if not point_based:
+            window_size *= 60
+
+        model = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom = model.fit_predict(
+            historical_data=ig_ts[:15],
+            data=ig_ts[15:],
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom.scores), len(ig_ts[15:]))
+
+        model1 = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom1 = model1.fit_predict(
+            data=ig_ts,
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom1.scores), len(ig_ts))
+
+    # pyre-ignore[56]
+    @parameterized.expand(
+        [
+            # score func, window, point_based, seasonality
+            ("z_score", 10, False, True),
+            ("z_score", 10, True, True),
+            ("z_score", 20, True, True),
+            ("z_score", 20, False, True),
+            ("MAD", 10, True, True),
+            ("MAD", 10, False, True),
+            ("IQR", 20, True, True),
+            ("IQR", 20, False, True),
+            ("modified_z_score_mad", 10, True, False),
+            ("modified_z_score_mad", 10, False, False),
+            ("modified_z_score_iqr", 20, True, False),
+            ("modified_z_score_iqr", 20, False, False),
+        ]
+    )
+    def test_data_with_individual_missing_datapoints(
+        self,
+        score_func: str,
+        window_size: int,
+        point_based: bool,
+        remove_seasonality: bool,
+    ) -> None:
+        ig_ts = generate_data_with_individual_missing_datapoints()
+
+        if not point_based:
+            window_size *= 60
+
+        model = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom = model.fit_predict(
+            historical_data=ig_ts[:15],
+            data=ig_ts[15:],
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom.scores), len(ig_ts[15:]))
+
+        model1 = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom1 = model1.fit_predict(
+            data=ig_ts,
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom1.scores), len(ig_ts))
+
+    # pyre-ignore[56]
+    @parameterized.expand(
+        [
+            # score func, window, point_based, seasonality
+            ("z_score", 10, False, False),
+            ("z_score", 10, True, False),
+            ("z_score", 20, True, False),
+            ("z_score", 20, False, False),
+            ("MAD", 10, True, False),
+            ("MAD", 10, False, False),
+            ("IQR", 20, True, False),
+            ("IQR", 20, False, False),
+            ("modified_z_score_mad", 10, True, False),
+            ("modified_z_score_mad", 10, False, False),
+            ("modified_z_score_iqr", 20, True, False),
+            ("modified_z_score_iqr", 20, False, False),
+        ]
+    )
+    def test_data_with_sudden_granularity_changes(
+        self,
+        score_func: str,
+        window_size: int,
+        point_based: bool,
+        remove_seasonality: bool,
+    ) -> None:
+        ig_ts = generate_data_with_sudden_granularity_changes()
+
+        if not point_based:
+            window_size *= 3600
+
+        model = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom = model.fit_predict(
+            historical_data=ig_ts[:15],
+            data=ig_ts[15:],
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom.scores), len(ig_ts[15:]))
+
+        model1 = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom1 = model1.fit_predict(
+            data=ig_ts,
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom1.scores), len(ig_ts))
+
+    # pyre-ignore[56]
+    @parameterized.expand(
+        [
+            # score func, window, point_based, seasonality
+            ("z_score", 9, False, False),
+            ("z_score", 9, True, False),
+            ("MAD", 10, True, False),
+            ("MAD", 10, False, False),
+            ("IQR", 10, True, False),
+            ("IQR", 10, False, False),
+            ("modified_z_score_mad", 9, True, False),
+            ("modified_z_score_mad", 9, False, False),
+            ("modified_z_score_iqr", 9, True, False),
+            ("modified_z_score_iqr", 9, False, False),
+        ]
+    )
+    def test_score_computation_func(
+        self,
+        score_func: str,
+        window_size: int,
+        point_based: bool,
+        remove_seasonality: bool,
+    ) -> None:
+        ts = generate_ts_data(length=10)
+
+        if not point_based:
+            window_size *= 3600
+
+        model = RollingStatsModel(
+            rolling_window=window_size,
+            statistics=score_func,
+            remove_seasonality=remove_seasonality,
+            point_based=point_based,
+        )
+
+        anom = model.fit_predict(
+            data=ts[9:],  # only 1 dp -> reorg_data has shape (1, X)
+            historical_data=ts[:9],
+        )
+        # prediction returns scores of same length
+        self.assertEqual(len(anom.scores), 1)
+
+
+class TestRollingStatsModelsError(TestCase):
+    def setUp(self) -> None:
+        self.ts = generate_ts_data()
+        time_index = pd.date_range(start="2018-01-01", freq="D", periods=2)
+        ts_pd = pd.DataFrame({"time": time_index, "val_1": [1, 2], "val_2": [1, 2]})
+        self.multi_ts = TimeSeriesData(ts_pd)
+
+    def test_window_size_error(self) -> None:
+        model = RollingStatsModel(
+            rolling_window=60,
+            point_based=False,
+        )
+        with self.assertRaises(ParameterError):
+            _ = model.fit_predict(data=self.ts)
+
+    def test_internal_error(self) -> None:
+        model = RollingStatsModel(
+            rolling_window=60,
+            point_based=False,
+        )
+        with self.assertRaises(InternalError):
+            _ = model.fit(data=self.ts)
+
+        with self.assertRaises(InternalError):
+            _ = model.predict(data=self.ts)
+
+    def test_data_error(self) -> None:
+        model = RollingStatsModel(
+            rolling_window=10,
+        )
+        with self.assertRaises(DataError):
+            _ = model.fit_predict(data=self.multi_ts)
+
+        with self.assertRaises(DataError):
+            _ = model.fit_predict(data=self.ts, historical_data=self.multi_ts)
