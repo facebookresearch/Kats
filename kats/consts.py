6ecabd2158efd6b56275c88d591d00181923624b
Peiyi Zhang <peiyizhang@meta.com>
2023-04-25 12:00:48 -0700

    Fix github
    Summary: ^
    
    Differential Revision:
    D45067972
    
    Privacy Context Container: L1153866
    
    fbshipit-source-id: 71c992d72396e6599705c057d9b8bcbe899f8de9

--- kats/consts.py
+++ kats/consts.py
@@ -272,10 +272,10 @@ class TimeSeriesData:
                     tz_nonexistent=tz_nonexistent,
                 )
                 # drop duplicate time stamps
-                df = df.drop_duplicates(subset=[self.time_col_name], keep="first")
-                logging.warning(
-                    "Drop duplicates in the time series if timestamps are identical."
-                )
+                # df = df.drop_duplicates(subset=[self.time_col_name], keep="first")
+                # logging.warning(
+                #     "Drop duplicates in the time series if timestamps are identical."
+                # )
 
                 # Sort by time
                 df = self._sort_by_time(sort_by_time=sort_by_time, df=df)
@@ -353,10 +353,10 @@ class TimeSeriesData:
 
             df = self.to_dataframe()
             # drop duplicate time stamps
-            df = df.drop_duplicates(subset=[self.time_col_name], keep="first")
-            logging.warning(
-                "Drop duplicates in the time series if timestamps are identical."
-            )
+            # df = df.drop_duplicates(subset=[self.time_col_name], keep="first")
+            # logging.warning(
+            #     "Drop duplicates in the time series if timestamps are identical."
+            # )
 
             df = self._sort_by_time(sort_by_time=sort_by_time, df=df)
             self._extract_from_df(df=df)
